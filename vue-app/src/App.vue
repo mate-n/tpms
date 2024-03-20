7f2ef4f08b5f2e5275ee4fdb3f6b98dfa1d6d034
Sandro Räss <s.raess@me.com>
2024-03-20 11:08:19 +0100

    Add App.vue.

---
+++ vue-app/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <v-app>
+    <!-- Must have the app property -->
+    <v-app-bar app :elevation="2">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon>
+          <v-icon color="green-darken-2" icon="mdi-home-heart"></v-icon>
+        </v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>tpms-frontend</v-app-bar-title>
+
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </v-app-bar>
+
+    <v-main> <RouterView /></v-main>
+  </v-app>
+</template>
