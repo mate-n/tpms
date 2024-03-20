a263631ad3854200a31a169aba5267cb6d13c152
Sandro Räss <s.raess@me.com>
2024-03-20 11:06:03 +0100

    Add views.

---
+++ vue-app/src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <v-icon icon="home-heart"></v-icon>
+    <v-icon color="green-darken-2" icon="mdi-home-heart" size="large"></v-icon>
+  </div>
+</template>
