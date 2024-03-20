3e9df8d93d2cfcb17827a8f24ba9963d1c70a4ba
Sandro Räss <s.raess@me.com>
2024-03-20 16:05:45 +0100

    Add translations and customLightTheme to main.ts.

--- vue-app/src/main.ts
+++ vue-app/src/main.ts
@@ -7,21 +7,46 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Theme
+const myCustomLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#f5f5f5'
+  }
+}
+
 // Vuetify
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-
 const vuetify = createVuetify({
   components,
-  directives
+  directives,
+  theme: {
+    defaultTheme: 'myCustomLightTheme',
+    themes: {
+      myCustomLightTheme
+    }
+  }
+})
+
+// Vue I18n
+import { createI18n } from 'vue-i18n'
+import { englishTranslations } from './translations/en'
+const messages = {
+  en: englishTranslations
+}
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages
 })
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
-
+app.use(i18n)
 app.mount('#app')
