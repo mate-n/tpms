1d635c376bdffee1ebf4c5302da9f6ad4a631316
Sandro Räss <s.raess@me.com>
2024-03-25 23:48:12 +0100

    Update cypress-test.sh.

--- cypress-test.sh
+++ cypress-test.sh
@@ -1,9 +1,9 @@
 cd vue-app
-apt-get update -y
-apt-get install libatk1.0-0 libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 npm install
 npm install -g wait-on
 #npm run serve & wait-on http://localhost:4173
+apt-get update -y
+apt-get apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 npm run dev -- --port 4173 & wait-on http://localhost:4173
 node_modules/.bin/cypress run --headless --spec "cypress/e2e/example.cy.ts"
 
