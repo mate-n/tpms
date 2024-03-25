fe3a5df5395e3c05f06246404ab1267c6057a3d8
Sandro Räss <s.raess@me.com>
2024-03-25 22:03:49 +0100

    Update api-test to use if-else.

--- api-test.sh
+++ api-test.sh
@@ -3,6 +3,10 @@ cd vue-app
 npm install
 #npm run api-test
 node src/test/selenium/compareJsonsFromMockServiceWithRealApi/profiles.spec.js
-rc=$?
-echo $rc
-[[ "$rc" -ne "0" ]] && exit(1)
+exit_code=$?
+if [[ $exit_code -eq 0 ]]; then
+    echo "Api-Test was successful"
+else
+    echo "Script failed with return code $exit_code"
+    exit(1)
+fi
