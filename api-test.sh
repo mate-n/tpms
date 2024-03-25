028a05e81e3877bf6183c0b2a201e0ac965ee4c1
Sandro Räss <s.raess@me.com>
2024-03-25 22:00:54 +0100

    Update api-test.sh to use node instead of mocha.

--- api-test.sh
+++ api-test.sh
@@ -2,7 +2,7 @@
 cd vue-app
 npm install
 #npm run api-test
-mocha src/test/selenium/compareJsonsFromMockServiceWithRealApi/profiles.spec.js
+node src/test/selenium/compareJsonsFromMockServiceWithRealApi/profiles.spec.js
 rc=$?
 echo $rc
 [[ "$rc" -ne "0" ]] && exit(1)
