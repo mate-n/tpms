7bdcee1415207f46d99d1f6a71c7bb7dd22f4f37
Sandro Räss <s.raess@me.com>
2024-03-25 20:58:30 +0100

    Add try-catch for profiles.spec.js to make mocha work.

---
+++ vue-app/src/test/selenium/compareJsonsFromMockServiceWithRealApi/profiles.spec.js
@@ -0,0 +1,16 @@
+import { Comparer } from "./Comparer.js";
+const comparer = new Comparer();
+
+async function compareResponses() {
+    try {
+        await comparer.compareApiResponses('?lookup&email=gert@ankerdata2.co.za', {});
+        await comparer.compareApiResponses('?read', {
+            "email": "gert@ankerdata1.co.za",
+            "personno": "585"
+        });
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+compareResponses();
