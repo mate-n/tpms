cb50df7af0bf8c5b264f8bdf3fb29966c477de57
Sandro Räss <s.raess@me.com>
2024-03-26 05:57:31 +0100

    Add failing test to test bitbucket-pipeline.

--- vue-app/cypress/e2e/example.cy.ts
+++ vue-app/cypress/e2e/example.cy.ts
@@ -6,3 +6,10 @@ describe('My First Test', () => {
     cy.contains('Welcome to our Website')
   })
 })
+
+describe('My Second Test', () => {
+  it('visits the app root url', () => {
+    cy.visit('/')
+    cy.contains('Welcome to our Websitexxxxx')
+  })
+})
