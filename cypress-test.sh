6dc6b8f58f978559135712e2f3840bf72b0b6452
Sandro Räss <s.raess@me.com>
2024-03-26 05:48:09 +0100

    Update cypress-test.sh.

--- cypress-test.sh
+++ cypress-test.sh
@@ -4,7 +4,10 @@ npm install -g wait-on
 #npm run serve & wait-on http://localhost:4173
 #apt-get update -y
 #apt-get apt-get install libatk1.0-0 libatk-bridge2.0-0 libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
-apt-get update -y && apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb libatk1.0-0 libatk-bridge2.0-dev
+apt-get update -y && apt-get install -y dbus libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb libatk1.0-0 libatk-bridge2.0-dev
+mkdir -p /var/run/dbus
+
+dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
 npm run dev -- --port 4173 & wait-on http://localhost:4173
 node_modules/.bin/cypress run --headless --spec "cypress/e2e/example.cy.ts"
 
