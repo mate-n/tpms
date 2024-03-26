459bf90fbd39bb65319aa4ff04600c363efa81b1
Sandro Räss <s.raess@me.com>
2024-03-26 06:13:07 +0100

    Remove failing test.

--- vue-app/cypress/e2e/example.cy.ts
+++ vue-app/cypress/e2e/example.cy.ts
@@ -6,10 +6,3 @@ describe('My First Test', () => {
     cy.contains('Welcome to our Website')
   })
 })
-
-describe('My Second Test', () => {
-  it('visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('Welcome to our Websitexxxxx')
-  })
-})
