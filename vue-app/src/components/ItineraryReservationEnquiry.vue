022786209cb89fe8eb1f3d1edbe2a763fd01ffcf
Metaphysiker <s.raess@me.com>
2024-03-27 20:45:37 +0100

    Update ItineraryReservationEnquiry to use Reservation.

--- vue-app/src/components/ItineraryReservationEnquiry.vue
+++ vue-app/src/components/ItineraryReservationEnquiry.vue
@@ -3,6 +3,11 @@ import { ref } from 'vue'
 import ProfileService from '@/services/ProfileService'
 import { inject } from 'vue'
 import type { AxiosStatic } from 'axios'
+import type { Ref } from 'vue'
+import type { IReservation } from '@/interfaces/IReservation'
+import { Reservation as ReservationClass } from '@/classes/Reservation'
+import Reservation from '@/components/Reservation.vue'
+
 const axios: AxiosStatic | undefined = inject('axios')
 const addReservationItems = [
   { title: 'New', icon: 'mdi-playlist-check' },
@@ -14,17 +19,19 @@ profileService.getProfile().then((profile) => {
   console.log(profile)
 })
 
-const arrivalDateString = getDateString(new Date())
-const arrivalDate = ref(new Date())
-const arrivalDateMenu = ref(false)
-const departureDateString = getDateString(new Date())
-const departureDate = ref(new Date())
-const departureDateMenu = ref(false)
+const reservations: Ref<IReservation[]> = ref([])
+const reservation1 = new ReservationClass()
+reservation1.camp = 'Twee Rivieren'
+reservation1.arrivalDate = new Date()
+reservation1.departureDate = new Date()
+reservation1.roomType = 'BD2'
+reservation1.rooms = 1
+reservation1.baseRateCategory = 'Base Rate | Low Season'
 
-function getDateString(date: Date): string {
-  const newDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
-  return newDate.toISOString().substring(0, 10)
-}
+const reservation2 = new ReservationClass()
+
+reservations.value.push(reservation1)
+reservations.value.push(reservation2)
 </script>
 
 <template>
@@ -50,125 +57,10 @@ function getDateString(date: Date): string {
       </v-list>
     </v-menu>
   </v-toolbar>
-  <v-container fluid class="bg-white">
-    <v-row class="d-flex align-center">
-      <v-col class="d-flex align-center h-100">
-        <v-select
-          label=""
-          :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
-        ></v-select>
-        <v-icon>mdi-city</v-icon>
-      </v-col>
-      <v-col>
-        <v-menu v-model="arrivalDateMenu" :close-on-content-click="false">
-          <template v-slot:activator="{ props }">
-            <v-text-field
-              v-model="arrivalDateString"
-              label="Arrival"
-              append-inner-icon="mdi-calendar"
-              v-bind="props"
-            ></v-text-field>
-          </template>
-          <v-card>
-            <v-date-picker :hide-header="true" v-model="arrivalDate"> </v-date-picker>
-            <v-spacer></v-spacer>
-            <div class="d-flex justify-space-between">
-              <v-btn @click="arrivalDateMenu = false"> Cancel </v-btn>
-              <v-btn
-                @click="(arrivalDateString = getDateString(arrivalDate)), (arrivalDateMenu = false)"
-              >
-                OK
-              </v-btn>
-            </div>
-          </v-card>
-        </v-menu>
-      </v-col>
-      <v-col>
-        <v-text-field label="Nights" type="number"></v-text-field>
-      </v-col>
-      <v-col>
-        <v-menu v-model="departureDateMenu" :close-on-content-click="false">
-          <template v-slot:activator="{ props }">
-            <v-text-field
-              v-model="departureDateString"
-              label="Departure"
-              append-inner-icon="mdi-calendar"
-              v-bind="props"
-            ></v-text-field>
-          </template>
-          <v-card>
-            <v-date-picker
-              :hide-header="true"
-              v-model="departureDate"
-              @update:model-value="departureDateString = getDateString(departureDate)"
-            ></v-date-picker>
-            <v-spacer></v-spacer>
-            <div class="d-flex justify-space-between">
-              <v-btn @click="departureDateMenu = false"> Cancel </v-btn>
-              <v-btn
-                @click="
-                  (arrivalDateString = getDateString(arrivalDate)), (departureDateMenu = false)
-                "
-              >
-                OK
-              </v-btn>
-            </div>
-          </v-card>
-        </v-menu>
-      </v-col>
-      <v-col>
-        <v-text-field label="Rooms" type="number"></v-text-field>
-      </v-col>
-      <v-col>
-        <v-autocomplete label="Room Type" :items="['Standard | King']" multiple></v-autocomplete>
-      </v-col>
-      <v-col>
-        <v-text-field label="Guests per room" type="number"></v-text-field>
-      </v-col>
-      <v-col>
-        <v-autocomplete
-          label="Guest"
-          placeholder="Last Name | First Name"
-          hint="Last Name | First Name"
-          :items="['Daniel, Oechslin']"
-        ></v-autocomplete>
-      </v-col>
-      <v-col class="d-flex justify-space-between">
-        <v-btn class="secondary-button mr-3">Reset</v-btn>
-        <v-btn class="primary-button">Check</v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
-  <v-container fluid class="bg-lightblue">
-    <v-row>
-      <v-col cols="4">
-        <v-autocomplete
-          label=""
-          prepend-inner-icon="mdi-magnify"
-          :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
-        ></v-autocomplete>
-      </v-col>
-    </v-row>
-    <v-row class="border-top">
-      <v-col cols="2" class="d-flex justify-space-between">
-        <v-icon class="text-primary">mdi-plus</v-icon>
-        Availibility (incl. OB)
-      </v-col>
-      <v-col class="bg-lightgray d-flex">
-        <div v-for="index in 12" :key="index" class="bg-white mr-3 px-5 py-2">
-          <v-icon>mdi-circle-small</v-icon>
-        </div>
-      </v-col>
-    </v-row>
-    <v-row class="border-top-bottom">
-      <v-col cols="2" class="d-flex justify-space-between"> </v-col>
-      <v-col class="bg-lightgray d-flex">
-        <div v-for="index in 12" :key="index" class="bg-white mr-3 px-5 py-2">
-          <v-icon>mdi-circle-small</v-icon>
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+
+  <template v-for="reservation of reservations">
+    <Reservation :reservation="reservation"></Reservation>
+  </template>
 
   <div class="d-flex justify-end">
     <v-btn class="secondary-button">Cancel</v-btn>
