15b29ec16f6d66b5e6a7f09eb559c54b5a99bd5f
Sandro Räss <s.raess@me.com>
2024-03-28 00:07:52 +0100

    Add getDateString to DateHelper.

--- vue-app/src/helpers/DateHelper.ts
+++ vue-app/src/helpers/DateHelper.ts
@@ -5,4 +5,9 @@ export class DateHelper {
     const timeDifference = departure.getTime() - arrival.getTime()
     return Math.ceil(timeDifference / (1000 * 3600 * 24))
   }
+
+  getDateString(date: Date): string {
+    const newDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
+    return newDate.toISOString().substring(0, 10)
+  }
 }
