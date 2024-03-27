f1c86d60b7ed26488a6e3497cf2cbb5c19f61fb8
Metaphysiker <s.raess@me.com>
2024-03-27 20:43:19 +0100

    Add IReservation.

---
+++ vue-app/src/interfaces/IReservation.ts
@@ -0,0 +1,12 @@
+export interface IReservation {
+  id: number
+  camp: string
+  arrivalDate: Date
+  departureDate: Date
+  rooms: number
+  roomType: string
+  guestsPerRoom: number
+  guest: string
+  baseRateCategory: string
+  orderIndex: number
+}
