f04f174f6fe83ea3a512dc4db6200f79fceb2301
Metaphysiker <s.raess@me.com>
2024-03-28 09:50:56 +0100

    Update ItineraryReservationEnquiry to perform a check for arrival- and departureDates.

--- vue-app/src/components/ItineraryReservationEnquiry.vue
+++ vue-app/src/components/ItineraryReservationEnquiry.vue
@@ -1,25 +1,56 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import ProfileService from '@/services/ProfileService'
-import { inject } from 'vue'
-import type { AxiosStatic } from 'axios'
+//import { inject } from 'vue'
+//import type { AxiosStatic } from 'axios'
 import type { Ref } from 'vue'
 import type { IReservation } from '@/interfaces/IReservation'
 import { Reservation as ReservationClass } from '@/classes/Reservation'
 import ReservationForm from '@/components/ReservationForm.vue'
+import { DateHelper } from '@/helpers/DateHelper'
 
-const axios: AxiosStatic | undefined = inject('axios')
-
-const profileService = new ProfileService(axios)
-profileService.getProfile().then((profile) => {
-  console.log(profile)
-})
+const dateHelper = new DateHelper()
+//const axios: AxiosStatic | undefined = inject('axios')
 
 const reservations: Ref<IReservation[]> = ref([])
 
-function addReservation() {
+const addReservation = () => {
+  const lastReservation = reservations.value[reservations.value.length - 1]
   const newReservation = new ReservationClass()
   reservations.value.push(newReservation)
+  newReservation.orderIndex = getNewOrderIndex()
+  if (lastReservation) {
+    newReservation.arrivalDate = lastReservation.departureDate
+    newReservation.departureDate = lastReservation.departureDate
+  }
+}
+
+const issues: Ref<string[]> = ref([])
+
+const getNewOrderIndex = () => {
+  if (reservations.value.length === 0) return 0
+  return reservations.value.length
+}
+
+const checkForIssues: () => void = () => {
+  issues.value = []
+
+  for (let i = 0; i < reservations.value.length; i++) {
+    if (i > 0) {
+      const isSameDay = dateHelper.isSameDay(
+        reservations.value[i].arrivalDate,
+        reservations.value[i - 1].departureDate
+      )
+      if (!isSameDay) {
+        issues.value.push('Reservation dates do not match up')
+      }
+    }
+  }
+
+  reservations.value.forEach((reservation, i) => {
+    if (!reservation.arrivalDate || !reservation.departureDate) {
+      issues.value.push(`Reservation ${i + 1} is missing dates`)
+    }
+  })
 }
 </script>
 
@@ -53,11 +84,19 @@ function addReservation() {
   </v-toolbar>
 
   <template v-for="(reservation, i) of reservations" :key="reservation.id">
-    <ReservationForm v-model="reservations[i]"></ReservationForm>
+    <ReservationForm v-model="reservations[i]" @check="checkForIssues()"></ReservationForm>
   </template>
 
-  <div class="d-flex justify-end mt-3">
-    <v-btn class="secondary-button">Cancel</v-btn>
-    <v-btn class="ml-2 disabled-button">Book</v-btn>
-  </div>
+  <v-container fluid>
+    <div class="my-3">
+      <div v-for="issue in issues" :key="issue">
+        <v-alert type="warning" elevation="2">{{ issue }}</v-alert>
+      </div>
+    </div>
+
+    <div class="d-flex justify-end mt-3">
+      <v-btn class="secondary-button">Cancel</v-btn>
+      <v-btn class="ml-2 disabled-button">Book</v-btn>
+    </div>
+  </v-container>
 </template>
