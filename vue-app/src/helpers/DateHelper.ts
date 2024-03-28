6bec28eae8ee1fc77569726f0f634fb6af8ff10f
Metaphysiker <s.raess@me.com>
2024-03-28 09:50:03 +0100

    Add isSameDay-method to DateHelper.

--- vue-app/src/helpers/DateHelper.ts
+++ vue-app/src/helpers/DateHelper.ts
@@ -10,4 +10,12 @@ export class DateHelper {
     const newDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
     return newDate.toISOString().substring(0, 10)
   }
+
+  isSameDay(d1: Date, d2: Date) {
+    return (
+      d1.getFullYear() === d2.getFullYear() &&
+      d1.getMonth() === d2.getMonth() &&
+      d1.getDate() === d2.getDate()
+    )
+  }
 }
