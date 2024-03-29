ce0d95ddc9d3279faebe3cce550988e08cd2a4ab
Sandro Räss <s.raess@me.com>
2024-03-29 21:06:25 +0100

    Fix errors given by eslint.

--- vue-app/src/components/ItineraryReservationEnquiry.vue
+++ vue-app/src/components/ItineraryReservationEnquiry.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
+import { computed, ref } from 'vue'
 
 // sandro.raess To-Do: Implement api with axios once api is available.
 //import { inject } from 'vue'
