f3bfbc5c2e431ed9d512d3895de107755f4778e3
Sandro Räss <s.raess@me.com>
2024-04-01 14:27:28 +0200

    Update ReservationValidator to use more descriptive functions.

--- vue-app/src/validators/ReservationValidator.ts
+++ vue-app/src/validators/ReservationValidator.ts
@@ -7,13 +7,25 @@ export class ReservationValidator implements IValidator {
 
   validate(reservation: IReservation): void {
     reservation.errors = {}
+    this.isGuestsPerRoomValid(reservation)
+    this.isRoomsValid(reservation)
+    this.isNightsValid(reservation)
+    this.isRoomTypeIsAvailable(reservation)
+  }
+
+  isGuestsPerRoomValid(reservation: IReservation): void {
     if (reservation.guestsPerRoom < 1) {
       reservation.errors['guestsPerRoom'] = 'Guests per room cannot be less than 1'
     }
+  }
+
+  isRoomsValid(reservation: IReservation): void {
     if (reservation.rooms < 1) {
       reservation.errors['rooms'] = 'Rooms cannot be less than 1'
     }
+  }
 
+  isNightsValid(reservation: IReservation): void {
     const nights = this.dateHelper.calculateNightsBetweenDates(
       reservation.arrivalDate,
       reservation.departureDate
@@ -21,6 +33,12 @@ export class ReservationValidator implements IValidator {
 
     if (nights < 1) {
       reservation.errors['nights'] = 'Nights cannot be less than 1'
+    }
+  }
+
+  isRoomTypeIsAvailable(reservation: IReservation): void {
+    if (reservation.roomType === 'Standard | King') {
+      reservation.errors['nights'] = 'Room Type is not available'
     }
   }
 }
