e6e2f783f707ccafb7ab11cdbe92f4494b9b4046
Metaphysiker <s.raess@me.com>
2024-04-05 11:23:58 +0200

    Add NewProfile.

---
+++ vue-app/src/components/profiles/NewProfile.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ProfileForm from './ProfileForm.vue'
+const emit = defineEmits(['close', 'profileSelected'])
+const close = () => emit('close')
+</script>
+<template>
+  <v-toolbar>
+    <v-toolbar-title>New Profile</v-toolbar-title>
+    <div class="border-s h-100 d-flex px-5 align-center" @click="close()">
+      <v-btn><v-icon>mdi-close</v-icon></v-btn>
+    </div>
+  </v-toolbar>
+  <div>
+    <ProfileForm></ProfileForm>
+  </div>
+</template>
