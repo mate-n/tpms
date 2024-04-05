e3200da932afc169b5e6cc797e5272f294e16677
Sandro Räss <s.raess@me.com>
2024-04-06 00:33:09 +0200

    Add avatar, save-button, personalSalutation, language, vip  and salutation to ProfileForm.

--- vue-app/src/components/profiles/ProfileForm.vue
+++ vue-app/src/components/profiles/ProfileForm.vue
@@ -4,14 +4,27 @@ import type { IProfile } from '@/interfaces/profiles/IProfile'
 import { ref } from 'vue'
 
 const newProfile = ref<IProfile>(new Profile())
+const save = () => {
+  console.log(newProfile.value)
+}
 </script>
 <template>
   <div class="d-flex">
-    <div>
-      <div class="color-avatar-tertiary text-white">O</div>
-      <div>PRIVATE</div>
+    <div style="flex-grow: 1" class="d-flex align-center justify-center">
+      <div>
+        <div class="d-flex justify-center">
+          <div>
+            <div class="color-avatar-tertiary text-white">
+              {{ newProfile.firstName.charAt(0) }}{{ newProfile.lastName.charAt(0) }}
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="border rounded mt-3 px-1">PRIVATE</div>
+        </div>
+      </div>
     </div>
-    <div class="flex-grow-1">
+    <div style="flex-grow: 4">
       <div class="d-flex">
         <v-text-field
           v-model="newProfile.lastName"
@@ -34,25 +47,25 @@ const newProfile = ref<IProfile>(new Profile())
       </div>
       <div class="d-flex">
         <v-text-field
-          v-model="newProfile.firstName"
+          v-model="newProfile.salutation"
           label="Salutation"
           variant="underlined"
           class="me-3"
         ></v-text-field>
         <v-text-field
-          v-model="newProfile.firstName"
+          v-model="newProfile.personalSalutation"
           label="Personal Salutation"
           variant="underlined"
           class="me-3"
         ></v-text-field>
         <v-text-field
-          v-model="newProfile.firstName"
+          v-model="newProfile.language"
           label="Language"
           variant="underlined"
           class="me-3"
         ></v-text-field>
         <v-text-field
-          v-model="newProfile.firstName"
+          v-model="newProfile.vip"
           label="VIP"
           variant="underlined"
           class="me-3"
@@ -60,4 +73,10 @@ const newProfile = ref<IProfile>(new Profile())
       </div>
     </div>
   </div>
+  <v-toolbar>
+    <v-toolbar-title></v-toolbar-title>
+    <div class="h-100 d-flex px-5 align-center" @click="save()">
+      <v-btn class="primary-button">SAVE</v-btn>
+    </div>
+  </v-toolbar>
 </template>
