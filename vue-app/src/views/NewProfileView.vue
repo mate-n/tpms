2dd66eec2561dc5b81a1df0c9e712603bc7dad74
Metaphysiker <s.raess@me.com>
2024-04-10 14:20:55 +0200

    Update NewProfileView to display empty profile in form.

--- vue-app/src/views/NewProfileView.vue
+++ vue-app/src/views/NewProfileView.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import { Profile } from '@/classes/Profile'
 import NewProfile from '@/components/profiles/NewProfile.vue'
+import type { IProfile } from '@/interfaces/profiles/IProfile'
+import { ref } from 'vue'
+const newProfile = ref<IProfile>(new Profile())
 </script>
 
 <template>
-  <NewProfile />
+  <NewProfile :profile-input="newProfile" />
 </template>
