b40e8cbc9a91896c464aeb585e90739ac54ecca1
Metaphysiker <s.raess@me.com>
2024-04-10 14:18:00 +0200

    Add put to ProfileService.

--- vue-app/src/services/ProfileService.ts
+++ vue-app/src/services/ProfileService.ts
@@ -36,6 +36,19 @@ class ProfileService implements IService {
     })
   }
 
+  put(profile: IProfile) {
+    return new Promise((resolve, reject) => {
+      this.axiosInstance
+        .put('profiles', profile)
+        .then((response: any) => {
+          resolve(response.data)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+
   search(profilePostBody: IProfilePostBody) {
     return new Promise<IProfile[]>((resolve, reject) => {
       this.axiosInstance
