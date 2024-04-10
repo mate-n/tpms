37611c1b0a76da100a5a52173c874f394bca642c
Metaphysiker <s.raess@me.com>
2024-04-10 14:13:59 +0200

    Add EditProfile.vue.

---
+++ vue-app/src/components/profiles/EditProfile.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import ProfileForm from './ProfileForm.vue'
+import type { IProfile } from '@/interfaces/profiles/IProfile'
+import { Profile } from '@/classes/Profile'
+import { CrudOperations } from '@/enums/CrudOperations'
+const emit = defineEmits(['close', 'update'])
+const close = () => emit('close')
+const isFullScreen = ref(false)
+const props = defineProps({
+  profileInput: { type: Object as () => IProfile, required: true }
+})
+const profileToBeEdited = ref<IProfile>(new Profile())
+const update = (profile: IProfile) => {
+  emit('update', profile)
+}
+onMounted(() => {
+  profileToBeEdited.value = props.profileInput.clone()
+})
+
+const toggleFullScreen = () => {
+  isFullScreen.value = !isFullScreen.value
+  const elem = document.documentElement
+  if (isFullScreen.value) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen()
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    }
+  }
+}
+</script>
+<template>
+  <v-toolbar>
+    <v-toolbar-title
+      >Profile <v-icon>mdi-arrow-right</v-icon> #{{ profileToBeEdited.id }}</v-toolbar-title
+    >
+    <div class="border-s h-100 d-flex px-5 align-center" @click="toggleFullScreen()">
+      <template v-if="isFullScreen">
+        <v-btn><v-icon>mdi-fullscreen-exit</v-icon></v-btn>
+      </template>
+      <template v-if="!isFullScreen">
+        <v-btn><v-icon>mdi-fullscreen</v-icon></v-btn>
+      </template>
+    </div>
+    <div class="border-s h-100 d-flex px-5 align-center" @click="close()">
+      <v-btn><v-icon>mdi-close</v-icon></v-btn>
+    </div>
+  </v-toolbar>
+  <div>
+    <ProfileForm
+      :profile-input="profileToBeEdited"
+      @save="(profile) => update(profile)"
+      :crud-operation="CrudOperations.Update"
+    ></ProfileForm>
+  </div>
+</template>
