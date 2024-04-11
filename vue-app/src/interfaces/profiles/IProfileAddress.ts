3b976103072449ae5d7ceed6685035a5342a5edb
Metaphysiker <s.raess@me.com>
2024-04-11 19:37:22 +0200

    Add ProfileAddress.

---
+++ vue-app/src/interfaces/profiles/IProfileAddress.ts
@@ -0,0 +1,12 @@
+export interface IProfileAddress {
+  id: number
+  type: string
+  recipient: string
+  country: string
+  postalCode: string
+  city: string
+  street1: string
+  street2: string
+  street3: string
+  mailingAddress: boolean
+}
