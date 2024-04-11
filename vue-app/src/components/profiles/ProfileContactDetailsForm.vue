20b926aadf82cb32cfff24a52cb805611e4551eb
Metaphysiker <s.raess@me.com>
2024-04-11 19:41:59 +0200

    Fix errors given by eslint.

--- vue-app/src/components/profiles/ProfileContactDetailsForm.vue
+++ vue-app/src/components/profiles/ProfileContactDetailsForm.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import type { IProfile } from '@/interfaces/profiles/IProfile'
 import ProfileContactDetailForm from './ProfileContactDetailForm.vue'
 import { ProfileContactDetail } from '@/classes/ProfileContactDetail'
 import { ref, type Ref } from 'vue'
