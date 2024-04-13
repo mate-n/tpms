eda8f76ba9c9b1ecef98529551c8e12b30aa02a6
Sandro Räss <sandro.raess@realms.ch>
2024-04-13 10:16:40 +0200

    Update IProfile to add all fields and associations needed for creating and editing profiles.

--- vue-app/src/interfaces/profiles/IProfile.ts
+++ vue-app/src/interfaces/profiles/IProfile.ts
@@ -5,56 +5,65 @@ export interface IProfile {
   firstName: string
   lastName: string
   middleName: string
-  salutation: string
-  personalSalutation: string
-  vip: string
-  language: string
-  activeStatus: string
-  profileType: string
-
   email: string
-  company: string
-  occupation: string
-  specialRates: number[]
-  licensePlate: string
-  passportNumber: string
-
-  dateOfBirth: Date
-  placeOfBirth: string
-  countryOfBirth: string
-
-  gender: string
-  nationality: string
-  country: string
-
-  city: string
+  language: string
+  phone: string
+  deibitorAccount: string
+  salesTaxNumber: string
+  salesTaxNumber2: string
+  salut: string
+  salutShort: string
+  vipCodeIDs: number[]
+  inactive: boolean
+  profileType: string
   isPasserby: boolean
 
+  //Communications / Contact details
+  communicationIDs: number[]
+
+  //Addresses
+  addressIDs: number[]
+
+  //Personal Info
+  birthPlace: string
+  birthCountry: string
+  birthday: Date
+  gender: string
+  nationality: string
+
+  //Additional Info
+  company: string
+  occupation: string
+  additionalInfoDefaultRateIDs: number[]
+  licensePlate: string
+  passport: string
+
   //Preferences / Notes
-  preferencesRoomIds: number[]
-  preferencesRoomNumber: number
-  preferencesRoomLanguage: string
-  preferencesNotes: string
+  preferencesNote: string
+  preferencesFeatureIDs: number[]
+  preferencesDefaultObjectID: number
+  preferencesLanguage: string
 
   //Marketing
-  marketingCodes: string[]
-  marketingTags: string[]
-  marketingAllowed: boolean
-  marketingDataProcessingAllowed: boolean
+  marketingCodeIDs: number[]
+  marketingTagIDs: number[]
+  allowMarketing: boolean
+  marketingDataProcessing: boolean
   marketingApprovalToPrintMembershipCard: boolean
 
-  //associations
-  contactDetailIds: number[]
-  adressIds: number[]
-  membershipCardIds: number[]
-  documentIds: number[]
-  attachmentIds: number[]
+  //Membership Cards
+  membershipCardIDs: number[]
+
+  //Documents
+  documentIDs: number[]
+
+  //Attachments
+  attachmentIDs: number[]
+
+  //Associations
   commisionIds: number[]
   stationeryIds: number[]
   invoiceIds: number[]
   reservationIds: number[]
   traceIds: number[]
-
-  clone(): IProfile
-  castToProfile(profile: IProfile): void
 }
