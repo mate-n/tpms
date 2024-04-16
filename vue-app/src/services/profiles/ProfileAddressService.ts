90a5d645a240f91515bb94e200ffbf5907ffe836
Metaphysiker <s.raess@me.com>
2024-04-16 12:40:58 +0200

    Add ProfileAddressService.

---
+++ vue-app/src/services/profiles/ProfileAddressService.ts
@@ -0,0 +1,25 @@
+import { AxiosInstanceFactory } from '@/factories/AxiosInstanceFactory'
+import type { IService } from '@/interfaces/IService'
+import type { IProfileAddress } from '@/interfaces/profiles/IProfileAddress'
+import type { IProfileAddressSearch } from '@/interfaces/profiles/IProfileAddressSearch'
+import type { AxiosStatic } from 'axios'
+
+export class ProfileAddressService implements IService {
+  axiosInstance: AxiosStatic
+  constructor(axiosInstance: AxiosStatic | undefined) {
+    this.axiosInstance = AxiosInstanceFactory.createAxiosInstance(axiosInstance)
+  }
+
+  search(profileAddressSearch: IProfileAddressSearch) {
+    return new Promise<IProfileAddress[]>((resolve, reject) => {
+      this.axiosInstance
+        .post('v1/profiles/addresses/search', profileAddressSearch)
+        .then((response: any) => {
+          resolve(response.data)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+}
