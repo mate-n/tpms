c01455ef9fd181bd844cc7449cca5604c40b5b37
Metaphysiker <s.raess@me.com>
2024-04-17 20:57:46 +0200

    Add NationalityService.

---
+++ vue-app/src/services/NationalityService.ts
@@ -0,0 +1,24 @@
+import { AxiosInstanceFactory } from '@/factories/AxiosInstanceFactory'
+import type { IGender } from '@/interfaces/IGender'
+import type { IService } from '@/interfaces/IService'
+import type { AxiosStatic } from 'axios'
+
+export class NationalityService implements IService {
+  axiosInstance: AxiosStatic
+  constructor(axiosInstance: AxiosStatic | undefined) {
+    this.axiosInstance = AxiosInstanceFactory.createAxiosInstance(axiosInstance)
+  }
+
+  getAvailableNationalities() {
+    return new Promise<IGender[]>((resolve, reject) => {
+      this.axiosInstance
+        .get('v1/available-nationalities')
+        .then((response: any) => {
+          resolve(response.data)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+}
