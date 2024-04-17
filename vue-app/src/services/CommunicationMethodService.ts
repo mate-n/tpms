36a81f0abf9d32ad795ac209f53fb50e6a884721
Sandro Räss <s.raess@me.com>
2024-04-18 01:03:12 +0200

    Add CommunicationMethodService.

---
+++ vue-app/src/services/CommunicationMethodService.ts
@@ -0,0 +1,24 @@
+import { AxiosInstanceFactory } from '@/factories/AxiosInstanceFactory'
+import type { ICommunicationMethod } from '@/interfaces/ICommunicationMethod'
+import type { IService } from '@/interfaces/IService'
+import type { AxiosStatic } from 'axios'
+
+export class CommunicationMethodService implements IService {
+  axiosInstance: AxiosStatic
+  constructor(axiosInstance: AxiosStatic | undefined) {
+    this.axiosInstance = AxiosInstanceFactory.createAxiosInstance(axiosInstance)
+  }
+
+  getAvailableCommunicationMethods() {
+    return new Promise<ICommunicationMethod[]>((resolve, reject) => {
+      this.axiosInstance
+        .get('v1/available-communication-methods')
+        .then((response: any) => {
+          resolve(response.data)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+}
