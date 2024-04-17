0f61450d1650fad8ac81c85ac89e7f597e7bece1
Metaphysiker <s.raess@me.com>
2024-04-17 21:07:50 +0200

    Update ProfileUserDefinedCard to use profiles-card-design.

--- vue-app/src/components/profiles/ProfileUserDefinedCard.vue
+++ vue-app/src/components/profiles/ProfileUserDefinedCard.vue
@@ -1,23 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <v-toolbar class="bg-white elevation-1">
-    <v-toolbar-title><span class="text-primary">User Defined</span></v-toolbar-title>
-    <v-btn icon>
-      <v-icon class="text-gray">mdi-pencil-outline</v-icon>
-    </v-btn>
-  </v-toolbar>
-  <v-divider></v-divider>
-  <v-card>
-    <template v-slot:text>
-      <div class="mb-2">
-        Mobile<br />
-        123456789
-      </div>
-      <div>
-        E-Mail<br />
-        max.mustermann@mail.com
-      </div>
-    </template>
-  </v-card>
+  <div class="profiles-card">
+    <v-toolbar class="profiles-card-toolbar">
+      <v-toolbar-title><span class="text-primary">User Defined</span></v-toolbar-title>
+      <v-btn icon class="text-gray"> <v-icon>mdi-account-box-outline</v-icon> || </v-btn>
+    </v-toolbar>
+
+    <v-divider class="profiles-card-divider"></v-divider>
+    <v-container> </v-container>
+  </div>
 </template>
