49531301b650cc34422ba256ca0bf411947b1073
Metaphysiker <s.raess@me.com>
2024-04-18 15:14:07 +0200

    Add getAllOfProfile, post, put, delete to ProfileCommunicationService.

--- vue-app/src/services/profiles/ProfileCommunicationService.ts
+++ vue-app/src/services/profiles/ProfileCommunicationService.ts
@@ -10,12 +10,66 @@ export class ProfileCommunicationService implements IService {
     this.axiosInstance = AxiosInstanceFactory.createAxiosInstance(axiosInstance)
   }
 
+  getAllOfProfile(profileID: number) {
+    return new Promise<IProfileCommunication[]>((resolve, reject) => {
+      this.axiosInstance
+        .get(`v1/profiles/${profileID}/communications`)
+        .then((response: any) => {
+          resolve(response.data)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+
+  post(profileCommunication: IProfileCommunication) {
+    return new Promise((resolve, reject) => {
+      this.axiosInstance
+        .post(`v1/profiles/${profileCommunication.profileID}/communications`, profileCommunication)
+        .then((response: any) => {
+          resolve(response.data)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+
+  put(profileCommunication: IProfileCommunication) {
+    return new Promise((resolve, reject) => {
+      this.axiosInstance
+        .put(`v1/profiles/${profileCommunication.profileID}/communications`, profileCommunication)
+        .then((response: any) => {
+          resolve(response.data)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+
   search(profileCommunicationSearch: IProfileCommunicationSearch) {
     return new Promise<IProfileCommunication[]>((resolve, reject) => {
       this.axiosInstance
         .post('v1/profiles/communications/search', profileCommunicationSearch)
         .then((response: any) => {
           resolve(response.data)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+
+  delete(profileCommunication: IProfileCommunication) {
+    return new Promise<void>((resolve, reject) => {
+      this.axiosInstance
+        .delete(
+          `v1/profiles/${profileCommunication.profileID}/communications/${profileCommunication.id}`
+        )
+        .then(() => {
+          resolve()
         })
         .catch((e: any) => {
           reject(e)
