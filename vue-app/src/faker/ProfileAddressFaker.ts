98c7d45200d1cc7a2d9f16ebdd487597b96f92c1
Metaphysiker <s.raess@me.com>
2024-04-23 19:48:26 +0200

    Update ProfileAddressFaker to use correct methods.

--- vue-app/src/faker/ProfileAddressFaker.ts
+++ vue-app/src/faker/ProfileAddressFaker.ts
@@ -4,22 +4,20 @@ import { faker } from '@faker-js/faker'
 
 export class ProfileAddressFaker {
   create(): IProfileAddress {
-    let profileAddress = new ProfileAddress()
-    const fakeData = {
-      id: faker.datatype.number(),
-      profileID: faker.datatype.number(),
-      type: faker.random.word(),
-      recipient: faker.person.fullName(),
-      country: faker.address.country(),
-      state: faker.address.state(),
-      zip: faker.address.zipCode(),
-      city: faker.address.city(),
-      street1: faker.address.streetAddress(),
-      street2: faker.address.streetAddress(),
-      street3: faker.address.streetAddress(),
-      mailingAddress: faker.datatype.boolean(),
-      primary: faker.datatype.boolean()
-    }
-    return (profileAddress = Object.assign(profileAddress, fakeData))
+    const profileAddress = new ProfileAddress()
+    profileAddress.id = faker.number.int()
+    profileAddress.profileID = faker.number.int()
+    profileAddress.typeString = faker.lorem.word()
+    profileAddress.recipient = faker.person.fullName()
+    profileAddress.country = faker.location.country()
+    profileAddress.state = faker.location.state()
+    profileAddress.zip = faker.location.zipCode()
+    profileAddress.city = faker.location.city()
+    profileAddress.street1 = faker.location.streetAddress()
+    profileAddress.street2 = faker.location.streetAddress()
+    profileAddress.street3 = faker.location.streetAddress()
+    profileAddress.mailingAddress = faker.datatype.boolean()
+    profileAddress.primary = faker.datatype.boolean()
+    return profileAddress
   }
 }
