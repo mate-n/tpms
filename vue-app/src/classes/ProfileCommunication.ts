785165fb47125cd04ce647baa8d728845b55f66c
Metaphysiker <s.raess@me.com>
2024-04-23 19:57:23 +0200

    Update ProfileCommunication to use IEntityWithIdentity.

--- vue-app/src/classes/ProfileCommunication.ts
+++ vue-app/src/classes/ProfileCommunication.ts
@@ -1,20 +1,20 @@
 import type { IProfileCommunication } from '@/interfaces/profiles/IProfileCommunication'
 
 export class ProfileCommunication implements IProfileCommunication {
-  id?: number
-  communicationTypeID: number
+  communicationTypeID: number | undefined
   typeID: number
   value: string
   primary: boolean
   description: string
   profileID: number
+  uniqueHash?: number
 
   constructor() {
-    this.communicationTypeID = 0
     this.typeID = 0
     this.value = ''
     this.primary = false
     this.description = ''
     this.profileID = 0
+    this.uniqueHash = new Date().getTime()
   }
 }
