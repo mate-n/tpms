83944066954a9c41207047c2617c21a5f864a869
Metaphysiker <s.raess@me.com>
2024-04-25 08:59:32 +0200

    Add BookableObjectService.

---
+++ vue-app/src/services/BookableObjectService.ts
@@ -0,0 +1,24 @@
+import { AxiosInstanceFactory } from '@/factories/AxiosInstanceFactory'
+import type { IBookableObject } from '@/interfaces/IBookableObject'
+import type { IService } from '@/interfaces/IService'
+import type { AxiosStatic } from 'axios'
+
+export class BookableObjectService implements IService {
+  axiosInstance: AxiosStatic
+  constructor(axiosInstance: AxiosStatic | undefined) {
+    this.axiosInstance = AxiosInstanceFactory.createAxiosInstance(axiosInstance)
+  }
+
+  getAvailableBookableObjects() {
+    return new Promise<IBookableObject[]>((resolve, reject) => {
+      this.axiosInstance
+        .get('v1/dropdowns/bookable-objects')
+        .then((response: any) => {
+          resolve(response.data)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+}
