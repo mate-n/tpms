09946dc21bd7e678b11af1b060eb9b129149d337
Metaphysiker <s.raess@me.com>
2024-04-25 20:06:54 +0200

    Update ProfilePreferencesForm to save profilePreferences after clicking on save.

--- vue-app/src/components/profiles/ProfilePreferencesForm.vue
+++ vue-app/src/components/profiles/ProfilePreferencesForm.vue
@@ -2,10 +2,12 @@
 import type { IBookableObject } from '@/interfaces/IBookableObject'
 import type { IFeature } from '@/interfaces/IFeature'
 import type { IProfile } from '@/interfaces/profiles/IProfile'
+import type { IProfilePreferencesPatchBody } from '@/interfaces/profiles/IProfilePreferencesPatchBody'
 import { BookableObjectService } from '@/services/BookableObjectService'
 import { FeatureService } from '@/services/FeatureService'
+import ProfileService from '@/services/ProfileService'
 import type { AxiosStatic } from 'axios'
-import { inject, onBeforeMount, ref } from 'vue'
+import { inject, onBeforeMount, ref, watch } from 'vue'
 const emits = defineEmits(['close'])
 const profileToBeEdited = defineModel({
   required: true,
@@ -14,6 +16,7 @@ const profileToBeEdited = defineModel({
 const showSaveButton = ref(false)
 const axios: AxiosStatic | undefined = inject('axios')
 const featureService = new FeatureService(axios)
+const profileService = new ProfileService(axios)
 const bookableObjectService = new BookableObjectService(axios)
 const featuresInDropdown = ref<IFeature[]>([])
 const bookableObjectsInDropdown = ref<IBookableObject[]>([])
@@ -26,12 +29,42 @@ onBeforeMount(() => {
     bookableObjectsInDropdown.value = response
   })
 })
+
+watch(
+  profileToBeEdited,
+  () => {
+    showSaveButton.value = true
+  },
+  { deep: true }
+)
+
+const saveProfilePreferences = () => {
+  if (!profileToBeEdited.value.id) return
+
+  const profilePreferencesPatchBody: IProfilePreferencesPatchBody = {
+    id: profileToBeEdited.value.id,
+    preferencesFeatureIDs: profileToBeEdited.value.preferencesFeatureIDs,
+    preferencesDefaultObjectID: profileToBeEdited.value.preferencesDefaultObjectID,
+    preferencesNote: profileToBeEdited.value.preferencesNote,
+    preferencesLanguage: profileToBeEdited.value.preferencesLanguage
+  }
+  profileService
+    .patchProfilePreferences(profilePreferencesPatchBody)
+    .then((response: IProfilePreferencesPatchBody) => {
+      profileToBeEdited.value = Object.assign(profileToBeEdited.value, response)
+      showSaveButton.value = false
+    })
+}
 </script>
 
 <template>
   <v-toolbar fluid class="profiles-card-toolbar">
     <v-toolbar-title>Profile Preferences </v-toolbar-title>
-    <div v-if="showSaveButton" class="profiles-card-toolbar-button text-primary">
+    <div
+      v-if="showSaveButton"
+      class="profiles-card-toolbar-button text-primary"
+      @click="saveProfilePreferences()"
+    >
       <v-icon size="large">mdi-content-save-outline</v-icon>
     </div>
     <div class="profiles-card-toolbar-button rounded-te" @click="emits('close')">
@@ -62,7 +95,6 @@ onBeforeMount(() => {
     <v-container fluid class="bg-white">
       <v-textarea
         v-model="profileToBeEdited.preferencesNote"
-        class="bg-lightgray"
         variant="underlined"
         label="Note"
       ></v-textarea>
