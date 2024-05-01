96fb025b96c033affb6f9e9d819dabe3449a3101
Metaphysiker <s.raess@me.com>
2024-05-01 15:47:21 +0200

    Add basketItemsStore.

---
+++ vue-app/src/stores/basketItems.ts
@@ -0,0 +1,27 @@
+import { IdentityHelper } from '@/helpers/IdentityHelper'
+import type { IReservation } from '@/interfaces/IReservation'
+import { defineStore } from 'pinia'
+const identityHelper = new IdentityHelper()
+
+export const useBasketItemsStore = defineStore('basketItems', {
+  state: () => ({ reservations: [] as IReservation[] }),
+  actions: {
+    addReservation(reservation: IReservation) {
+      if (!this.searchReservation(reservation)) {
+        this.reservations.push(reservation)
+      }
+    },
+    removeReservation(reservation: IReservation) {
+      this.reservations = this.reservations.filter(
+        (reservationInBasket) => !identityHelper.isSame(reservationInBasket, reservation)
+      )
+    },
+    searchReservation(reservation: IReservation) {
+      return identityHelper.findByIdOrUniqueHash(
+        this.reservations,
+        reservation.id,
+        reservation.uniqueHash
+      )
+    }
+  }
+})
