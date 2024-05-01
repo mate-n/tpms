362520c18f5b9033dc42f4ef7b0948e87700ae9a
Metaphysiker <s.raess@me.com>
2024-05-01 08:52:59 +0200

    Rename getMyProperty to getLocalProperty.

--- vue-app/src/services/PropertyService.ts
+++ vue-app/src/services/PropertyService.ts
@@ -22,10 +22,10 @@ export class PropertyService implements IService {
     })
   }
 
-  getMyProperty() {
+  getLocalProperty() {
     return new Promise<IProperty>((resolve, reject) => {
       this.axiosInstance
-        .get('v1/properties/get-my-property')
+        .get('v1/properties/local-property')
         .then((response: any) => {
           resolve(response.data)
         })
