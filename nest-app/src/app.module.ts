2bcffdb4f504368de254447202733a88e87d18de
Metaphysiker <s.raess@me.com>
2024-05-03 10:14:30 +0200

    Add ProfileAddressesService to app.module.ts.

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -5,10 +5,17 @@ import { ProfilesModule } from './profiles/v1/profiles.module';
 import { PropertiesModule } from './properties/v1/properties.module';
 import { DropdownsController } from './dropdowns/v1/dropdowns.controller';
 import { RoomsController } from './rooms/v1/rooms.controller';
+import { ProfileAddressesController } from './profile-addresses/profile-addresses.controller';
+import { ProfileAddressesService } from './profile-addresses/profile-addresses.service';
 
 @Module({
   imports: [ProfilesModule, PropertiesModule],
-  controllers: [AppController, DropdownsController, RoomsController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    DropdownsController,
+    RoomsController,
+    ProfileAddressesController,
+  ],
+  providers: [AppService, ProfileAddressesService],
 })
 export class AppModule {}
