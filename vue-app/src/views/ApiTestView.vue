bcb22757b5da28e4a05f8b384ff2bdd9960028b5
Sandro Räss <s.raess@me.com>
2024-05-03 19:06:24 +0200

    Remove obsolete ProfileDocument.

--- vue-app/src/views/ApiTestView.vue
+++ vue-app/src/views/ApiTestView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ProfileAddressFaker } from '@/shared/faker/ProfileAddressFaker'
 import { ProfileCommunicationFaker } from '@/shared/faker/ProfileCommunicationFaker'
-import { ProfileDocumentFaker } from '@/shared/faker/ProfileDocumentFaker'
 import { ProfilesFaker } from '@/shared/faker/ProfilesFaker'
 import { PropertyAvailabilityFaker } from '@/shared/faker/PropertyAvailabilityFaker'
 import { RoomFaker } from '@/shared/faker/RoomFaker'
@@ -26,13 +25,6 @@ const profileCommunicationFake2 = profileCommunicationFaker.create()
 const profileCommunicationFake3 = profileCommunicationFaker.create()
 console.log(profileCommunicationFake1)
 console.log([profileCommunicationFake1, profileCommunicationFake2, profileCommunicationFake3])
-
-const profileDocumentFaker = new ProfileDocumentFaker()
-const profileDocumentFake1 = profileDocumentFaker.create()
-const profileDocumentFake2 = profileDocumentFaker.create()
-const profileDocumentFake3 = profileDocumentFaker.create()
-console.log(profileDocumentFake1)
-console.log([profileDocumentFake1, profileDocumentFake2, profileDocumentFake3])
 
 const propertyAvailabilityFaker = new PropertyAvailabilityFaker()
 const propertyAvailabilityFake1 = propertyAvailabilityFaker.create()
