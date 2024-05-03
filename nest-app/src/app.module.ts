afebbfad03a596f239a51f450acd892fc7e4c973
Sandro Räss <s.raess@me.com>
2024-05-03 19:10:32 +0200

    Update app.module to add new controllers and services.

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -5,8 +5,9 @@ import { ProfilesModule } from './profiles/v1/profiles.module';
 import { PropertiesModule } from './properties/v1/properties.module';
 import { DropdownsController } from './dropdowns/v1/dropdowns.controller';
 import { RoomsController } from './rooms/v1/rooms.controller';
-import { ProfileAddressesController } from './profile-addresses/profile-addresses.controller';
-import { ProfileAddressesService } from './profile-addresses/profile-addresses.service';
+import { ProfileAddressesController } from './profile-addresses/v1/profile-addresses.controller';
+import { ProfileAddressesService } from './profile-addresses/v1/profile-addresses.service';
+import { ProfileCommunicationsController } from './profile-communications/v1/profile-communications.controller';
 
 @Module({
   imports: [ProfilesModule, PropertiesModule],
@@ -15,6 +16,7 @@ import { ProfileAddressesService } from './profile-addresses/profile-addresses.s
     DropdownsController,
     RoomsController,
     ProfileAddressesController,
+    ProfileCommunicationsController,
   ],
   providers: [AppService, ProfileAddressesService],
 })
