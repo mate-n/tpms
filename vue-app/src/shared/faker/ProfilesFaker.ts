cd2c8a1003d6ccee4b81ec29a1e0ec626a0b99be
Metaphysiker <s.raess@me.com>
2024-05-03 09:24:44 +0200

    Update ProfilesFaker to remove fakeData.

--- vue-app/src/shared/faker/ProfilesFaker.ts
+++ vue-app/src/shared/faker/ProfilesFaker.ts
@@ -5,53 +5,51 @@ import { Profile } from '../classes/Profile'
 export class ProfilesFaker {
   create(): IProfile {
     let profile = new Profile()
-    const fakeData = {
-      id: faker.number.int(),
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
-      middleName: faker.person.middleName(),
-      email: faker.internet.email(),
-      language: faker.location.countryCode(),
-      phone: faker.phone.number(),
-      deibitorAccount: faker.finance.accountNumber(),
-      salesTaxNumber: faker.finance.accountNumber(),
-      salesTaxNumber2: faker.finance.accountNumber(),
-      salut: faker.person.prefix(),
-      salutShort: faker.person.prefix(),
-      vipCodeIDs: [faker.number.int(), faker.number.int()],
-      inactive: faker.datatype.boolean(),
-      profileType: faker.lorem.word(),
-      isPasserby: faker.datatype.boolean(),
-      communicationIDs: [faker.number.int(), faker.number.int()],
-      addressIDs: [faker.number.int(), faker.number.int()],
-      birthPlace: faker.location.city(),
-      birthCountry: faker.location.country(),
-      birthday: faker.date.past(),
-      gender: faker.person.gender(),
-      nationality: faker.location.countryCode(),
-      company: faker.company.name(),
-      occupation: faker.person.jobTitle(),
-      additionalInfoDefaultRateIDs: [faker.number.int(), faker.number.int()],
-      licensePlate: faker.vehicle.vin(),
-      passport: faker.number.int(),
-      preferencesNote: faker.lorem.sentence(),
-      preferencesFeatureIDs: [faker.number.int(), faker.number.int()],
-      preferencesDefaultObjectID: faker.number.int(),
-      preferencesLanguage: faker.location.countryCode(),
-      marketingCodeIDs: [faker.number.int(), faker.number.int()],
-      marketingTagIDs: [faker.number.int(), faker.number.int()],
-      allowMarketing: faker.datatype.boolean(),
-      marketingDataProcessing: faker.datatype.boolean(),
-      marketingApprovalToPrintMembershipCard: faker.datatype.boolean(),
-      membershipCardIDs: [faker.number.int(), faker.number.int()],
-      documentIDs: [faker.number.int(), faker.number.int()],
-      attachmentIDs: [faker.number.int(), faker.number.int()],
-      commisionIds: [faker.number.int(), faker.number.int()],
-      stationeryIds: [faker.number.int(), faker.number.int()],
-      invoiceIds: [faker.number.int(), faker.number.int()],
-      reservationIds: [faker.number.int(), faker.number.int()],
-      traceIds: [faker.number.int(), faker.number.int()]
-    }
-    return (profile = Object.assign(profile, fakeData))
+    profile.id = faker.number.int()
+    profile.firstName = faker.person.firstName()
+    profile.lastName = faker.person.lastName()
+    profile.middleName = faker.person.middleName()
+    profile.email = faker.internet.email()
+    profile.language = faker.location.countryCode()
+    profile.phone = faker.phone.number()
+    profile.deibitorAccount = faker.finance.accountNumber()
+    profile.salesTaxNumber = faker.finance.accountNumber()
+    profile.salesTaxNumber2 = faker.finance.accountNumber()
+    profile.salut = faker.person.prefix()
+    profile.salutShort = faker.person.prefix()
+    profile.vipCodeIDs = [faker.number.int(), faker.number.int()]
+    profile.inactive = faker.datatype.boolean()
+    profile.profileType = faker.lorem.word()
+    profile.isPasserby = faker.datatype.boolean()
+    profile.communicationIDs = [faker.number.int(), faker.number.int()]
+    profile.addressIDs = [faker.number.int(), faker.number.int()]
+    profile.birthPlace = faker.location.city()
+    profile.birthCountry = faker.location.country()
+    profile.birthday = faker.date.past()
+    profile.gender = faker.person.gender()
+    profile.nationality = faker.location.countryCode()
+    profile.company = faker.company.name()
+    profile.occupation = faker.person.jobTitle()
+    profile.additionalInfoDefaultRateIDs = [faker.number.int(), faker.number.int()]
+    profile.licensePlate = faker.vehicle.vin()
+    profile.passport = faker.number.int().toString()
+    profile.preferencesNote = faker.lorem.sentence()
+    profile.preferencesFeatureIDs = [faker.number.int(), faker.number.int()]
+    profile.preferencesDefaultObjectID = faker.number.int()
+    profile.preferencesLanguage = faker.location.countryCode()
+    profile.marketingCodeIDs = [faker.number.int(), faker.number.int()]
+    profile.marketingTagIDs = [faker.number.int(), faker.number.int()]
+    profile.allowMarketing = faker.datatype.boolean()
+    profile.marketingDataProcessing = faker.datatype.boolean()
+    profile.marketingApprovalToPrintMembershipCard = faker.datatype.boolean()
+    profile.membershipCardIDs = [faker.number.int(), faker.number.int()]
+    profile.documentIDs = [faker.number.int(), faker.number.int()]
+    profile.attachmentIDs = [faker.number.int(), faker.number.int()]
+    profile.commisionIds = [faker.number.int(), faker.number.int()]
+    profile.stationeryIds = [faker.number.int(), faker.number.int()]
+    profile.invoiceIds = [faker.number.int(), faker.number.int()]
+    profile.reservationIds = [faker.number.int(), faker.number.int()]
+    profile.traceIds = [faker.number.int(), faker.number.int()]
+    return profile
   }
 }
