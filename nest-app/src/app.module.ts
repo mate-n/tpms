31cb932d73ca8b57479d1e524257aea9f653d08a
Metaphysiker <s.raess@me.com>
2024-05-03 09:27:34 +0200

    Add controllers to app.module.ts.

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfilesModule } from './profiles/v1/profiles.module';
 import { PropertiesModule } from './properties/v1/properties.module';
+import { DropdownsController } from './dropdowns/v1/dropdowns.controller';
+import { RoomsController } from './rooms/v1/rooms.controller';
 
 @Module({
   imports: [ProfilesModule, PropertiesModule],
-  controllers: [AppController],
+  controllers: [AppController, DropdownsController, RoomsController],
   providers: [AppService],
 })
 export class AppModule {}
