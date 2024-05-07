7ee89d3551c247d375c50038598414f6396bb671
Sandro Räss <s.raess@me.com>
2024-05-07 08:36:29 +0200

    Update port in nest-app to have different port than vue-app.

--- nest-app/src/main.ts
+++ nest-app/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
-  await app.listen(3000);
+  await app.listen(3001);
 
   if (module.hot) {
     module.hot.accept();
