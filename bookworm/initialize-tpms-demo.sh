702ee3ac0221ac8415a8b5e8f164b6f04ffc4682
Metaphysiker <s.raess@me.com>
2024-05-07 13:21:06 +0200

    Use nvm instead of nvm.sh in intialize-tpms-demo.sh.

--- bookworm/initialize-tpms-demo.sh
+++ bookworm/initialize-tpms-demo.sh
@@ -2,9 +2,8 @@
 touch ~/.bashrc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
-~/.nvm/nvm.sh install 21.0.0
-#nvm install 21.0.0
-~/.nvm/nvm.sh use 21.0.0
+nvm install 21.0.0
+nvm use 21.0.0
 npm install pm2@latest -g
 export VITE_API_BASE_URL=http://localhost:3001/
 
