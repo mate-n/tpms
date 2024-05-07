2b2add168a34d42c9cebe91a38e5d9c74b3a22cf
Metaphysiker <s.raess@me.com>
2024-05-07 09:18:20 +0200

    Update tpms-demo-setup.sh.

--- bookworm/tpms-demo-setup.sh
+++ bookworm/tpms-demo-setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 apt-get update
-apt-get install git vim openssh-client procps -y
-ssh -V
-eval $(ssh-agent)
-mkdir .ssh
-scp tpms-shared-documents@159.223.30.119:tpms-frontend.pub .ssh
-scp tpms-shared-documents@159.223.30.119:tpms-frontend .ssh
-ssh-add .ssh/tpms-frontend
-git clone git@bitbucket.org:\_realms/tpms-frontend.git
+apt-get install git -y
+git clone https://ankerdata_tpms@bitbucket.org/_realms/tpms-frontend.git
 cd tpms-frontend
 git fetch
-git checkout -b develop origin/develop
+git checkout -b feature/tpms-68-CreateScriptforDemo origin/feature/tpms-68-CreateScriptforDemo
+/bin/bash copy-relevant-files-to-target-folder.sh ../tpms-frontend-demo
+cd ../tpms-frontend-demo
