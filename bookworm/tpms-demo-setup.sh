899c2f76f9a59a9cb3547ae69c4d77d683e8ef6a
Metaphysiker <s.raess@me.com>
2024-05-07 10:22:17 +0200

    Remove obsolete tpms-demo-setup.sh.

--- bookworm/tpms-demo-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install git -y
-git clone https://ankerdata_tpms@bitbucket.org/_realms/tpms-frontend.git
-cd tpms-frontend
-git fetch
-git checkout -b feature/tpms-68-CreateScriptforDemo origin/feature/tpms-68-CreateScriptforDemo
-/bin/bash copy-relevant-files-to-target-folder.sh ../tpms-frontend-demo
-cd ../tpms-frontend-demo
