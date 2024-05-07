18412174a7415a8bdb9233ac3b860258503ea0a2
Sandro Räss <s.raess@me.com>
2024-05-07 08:50:33 +0200

    Add tpms-demo.config.js.

---
+++ bookworm/tpms-demo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+        name: "vue-app",
+        script: "../vue-app/vue-app-express.cjs"
+    }, {
+        name: "nest-app",
+        script: "../nest-app/dist/main.js"
+    }]
+}
