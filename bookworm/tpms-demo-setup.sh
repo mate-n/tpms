fe83b0927fd99758477a107e48afcda3d52e0306
Metaphysiker <s.raess@me.com>
2024-05-07 13:48:53 +0200

    Update tpms-demo-setup.sh to use correct branch for demo.

--- bookworm/tpms-demo-setup.sh
+++ bookworm/tpms-demo-setup.sh
@@ -2,13 +2,10 @@
 TPMS_DEMO_DIR=/home/$(whoami)/realms
 
 mkdir -p $TPMS_DEMO_DIR/tpms-frontend-demo
-# The following command must run in Raspberry Pi.
-#sudo apt-get update && sudo apt-get install git -y
 cd $TPMS_DEMO_DIR
 git clone https://ankerdata_tpms:ATBBbUP6LFDmChmSMcJn9pWVcT6K3F3EF006@bitbucket.org/\_realms/tpms-frontend.git
 cd $TPMS_DEMO_DIR/tpms-frontend
-git checkout -b feature/tpms-68-CreateScriptforDemo origin/feature/tpms-68-CreateScriptforDemo
-#git checkout -b demo origin/demo
+git checkout -b demo origin/demo
 cd $TPMS_DEMO_DIR/tpms-frontend/bookworm
 /bin/bash copy-relevant-files-to-target-folder.sh $TPMS_DEMO_DIR/tpms-frontend-demo
 cd $TPMS_DEMO_DIR/tpms-frontend-demo/bookworm
