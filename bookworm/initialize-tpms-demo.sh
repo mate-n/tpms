c3f707ddf6cca18a9e1c580dbf9480861cbd45bc
Metaphysiker <s.raess@me.com>
2024-05-07 10:10:55 +0200

    Update initialize-tpms-demo to add environment-variable to vue-app.

--- bookworm/initialize-tpms-demo.sh
+++ bookworm/initialize-tpms-demo.sh
@@ -6,8 +6,7 @@ source ~/.bashrc
 nvm install 21.0.0
 nvm use 21.0.0
 npm install pm2@latest -g
-
-mkdir /app
+export VITE_API_BASE_URL=http://localhost:3001/
 
 /bin/bash initialize-nest-app.sh
 /bin/bash initialize-vue-app.sh
