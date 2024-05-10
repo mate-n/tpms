e9daec571e279053e4b7eb0735bf023fd34af4ac
Sandro Räss <s.raess@me.com>
2024-05-10 21:48:07 +0200

    Update ProfileCommunicationValidator to check if email is valid.

--- vue-app/src/shared/validators/ProfileCommunicationValidator.ts
+++ vue-app/src/shared/validators/ProfileCommunicationValidator.ts
@@ -1,7 +1,22 @@
+import { CommunicationMethodService } from '@/services/CommunicationMethodService'
 import type { IValidator } from '../interfaces/IValidator'
 import type { IProfileCommunication } from '../interfaces/profiles/IProfileCommunication'
+import type { AxiosStatic } from 'axios'
+import { inject } from 'vue'
+const axios: AxiosStatic | undefined = inject('axios')
 
 export class ProfileCommunicationValidator implements IValidator {
+  communicationMethodService = new CommunicationMethodService(axios)
+
+  validatePromise(profileCommunicationToBeValidated: IProfileCommunication): Promise<void> {
+    return new Promise((finalResolve) => {
+      profileCommunicationToBeValidated.errors = {}
+      this.isEmailValidPromise(profileCommunicationToBeValidated).then(() => {
+        finalResolve()
+      })
+    })
+  }
+
   emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
   validate(profileCommunicationToBeValidated: IProfileCommunication): void {
     profileCommunicationToBeValidated.errors = {}
@@ -15,5 +30,26 @@ export class ProfileCommunicationValidator implements IValidator {
         profileCommunicationToBeValidated.errors!['value'] = 'Invalid email address'
       }
     }
+  }
+
+  isEmailValidPromise(profileCommunicationToBeValidated: IProfileCommunication): Promise<void> {
+    return new Promise((finalResolve) => {
+      console.log(profileCommunicationToBeValidated)
+      if (profileCommunicationToBeValidated.communicationTypeID) {
+        const getCommunicationMethodPromise = this.communicationMethodService
+          .get(profileCommunicationToBeValidated.communicationTypeID)
+          .then((communicationMethods) => {
+            console.log(communicationMethods)
+          })
+      }
+
+      if (profileCommunicationToBeValidated.communicationTypeID === 147) {
+        const isEmailValid = this.emailRegex.test(profileCommunicationToBeValidated.value)
+        if (!isEmailValid) {
+          profileCommunicationToBeValidated.errors!['value'] = 'Invalid email address'
+        }
+      }
+      finalResolve()
+    })
   }
 }
