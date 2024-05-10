a84a1e4ac73b8845029dac44db92f8b7659db064
Sandro Räss <s.raess@me.com>
2024-05-10 20:06:54 +0200

    Update IProfileCommunication to implement IEntityWithErrors.

--- vue-app/src/shared/interfaces/profiles/IProfileCommunication.ts
+++ vue-app/src/shared/interfaces/profiles/IProfileCommunication.ts
@@ -1,6 +1,7 @@
+import type { IEntityWithErrors } from '../IEntityWithErrors'
 import type { IEntityWithIdentity } from '../IEntityWithIdentity'
 
-export interface IProfileCommunication extends IEntityWithIdentity {
+export interface IProfileCommunication extends IEntityWithIdentity, IEntityWithErrors {
   communicationTypeID: number | undefined
   typeID: number
   value: string
