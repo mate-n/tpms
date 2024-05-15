fa8d512ed59297c4a8377754da25dc3476a386e3
Sandro Räss <s.raess@me.com>
2024-05-15 10:53:42 +0200

    Add isValueEmpty to ProfileCommunicationValidator.

--- vue-app/src/shared/validators/ProfileCommunicationValidator.ts
+++ vue-app/src/shared/validators/ProfileCommunicationValidator.ts
@@ -5,11 +5,18 @@ export class ProfileCommunicationValidator implements IValidator {
   emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
   validate(profileCommunicationToBeValidated: IProfileCommunication): void {
     profileCommunicationToBeValidated.errors = {}
+    this.isValueEmpty(profileCommunicationToBeValidated)
     this.isEmailValid(profileCommunicationToBeValidated)
   }
 
+  isValueEmpty(profileCommunicationToBeValidated: IProfileCommunication): void {
+    if (profileCommunicationToBeValidated.value === '') {
+      profileCommunicationToBeValidated.errors!['value'] = 'Value cannot be empty'
+    }
+  }
+
   isEmailValid(profileCommunicationToBeValidated: IProfileCommunication): void {
-    if (profileCommunicationToBeValidated.communicationTypeID === 147) {
+    if (profileCommunicationToBeValidated.communicationTypeName === 'E-Mail') {
       const isEmailValid = this.emailRegex.test(profileCommunicationToBeValidated.value)
       if (!isEmailValid) {
         profileCommunicationToBeValidated.errors!['value'] = 'Invalid email address'
