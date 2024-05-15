060806b15c84a081c49e6a6ec6419388849888f9
Sandro Räss <s.raess@me.com>
2024-05-15 10:53:27 +0200

    Add communicationTypeName to IProfileCommunication and ProfileCommunication.

--- vue-app/src/shared/interfaces/profiles/IProfileCommunication.ts
+++ vue-app/src/shared/interfaces/profiles/IProfileCommunication.ts
@@ -3,6 +3,7 @@ import type { IEntityWithIdentity } from '../IEntityWithIdentity'
 
 export interface IProfileCommunication extends IEntityWithIdentity, IEntityWithErrors {
   communicationTypeID: number | undefined
+  communicationTypeName: string
   typeID: number
   value: string
   primary: boolean
