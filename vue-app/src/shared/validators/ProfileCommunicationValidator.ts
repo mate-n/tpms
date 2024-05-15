d3ae847c3c8412a526a8d1ee81a818dcc788a883
Sandro Räss <s.raess@me.com>
2024-05-15 11:53:00 +0200

    Add isCommunicationTypeIDValid to ProfileCommunicationValidator.

--- vue-app/src/shared/validators/ProfileCommunicationValidator.ts
+++ vue-app/src/shared/validators/ProfileCommunicationValidator.ts
@@ -5,13 +5,21 @@ export class ProfileCommunicationValidator implements IValidator {
   emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
   validate(profileCommunicationToBeValidated: IProfileCommunication): void {
     profileCommunicationToBeValidated.errors = {}
-    this.isValueEmpty(profileCommunicationToBeValidated)
+    this.isValueValid(profileCommunicationToBeValidated)
     this.isEmailValid(profileCommunicationToBeValidated)
+    this.isCommunicationTypeIDValid(profileCommunicationToBeValidated)
   }
 
-  isValueEmpty(profileCommunicationToBeValidated: IProfileCommunication): void {
+  isValueValid(profileCommunicationToBeValidated: IProfileCommunication): void {
     if (profileCommunicationToBeValidated.value === '') {
       profileCommunicationToBeValidated.errors!['value'] = 'Value cannot be empty'
+    }
+  }
+
+  isCommunicationTypeIDValid(profileCommunicationToBeValidated: IProfileCommunication): void {
+    if (profileCommunicationToBeValidated.communicationTypeID === undefined) {
+      profileCommunicationToBeValidated.errors!['communicationTypeID'] =
+        'Communication type cannot be empty'
     }
   }
 
