214d9fa9ce8573f1bf009da10884310b36e76c26
Sandro Räss <s.raess@me.com>
2024-05-15 12:19:19 +0200

    Add isTypeStringValid and isCountryValid to ProfileAddressValidator.

--- vue-app/src/shared/validators/ProfileAddressValidator.ts
+++ vue-app/src/shared/validators/ProfileAddressValidator.ts
@@ -2,8 +2,21 @@ import type { IValidator } from '../interfaces/IValidator'
 import type { IProfileAddress } from '../interfaces/profiles/IProfileAddress'
 
 export class ProfileAddressValidator implements IValidator {
-  validate(objectToBeValidated: IProfileAddress): void {
-    console.log(objectToBeValidated)
-    throw new Error('Method not implemented.')
+  validate(profileAddressToBeValidated: IProfileAddress): void {
+    profileAddressToBeValidated.errors = {}
+    this.isTypeStringValid(profileAddressToBeValidated)
+    this.isCountryValid(profileAddressToBeValidated)
+  }
+
+  isTypeStringValid(profileAddressToBeValidated: IProfileAddress): void {
+    if (profileAddressToBeValidated.typeString === '') {
+      profileAddressToBeValidated.errors!['typeString'] = 'Type cannot be empty'
+    }
+  }
+
+  isCountryValid(profileAddressToBeValidated: IProfileAddress): void {
+    if (profileAddressToBeValidated.country === '') {
+      profileAddressToBeValidated.errors!['country'] = 'Country cannot be empty'
+    }
   }
 }
