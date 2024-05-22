475e50d413aa76be1c5fef812f7a26f0a890e379
Sandro Räss <s.raess@me.com>
2024-05-22 21:31:19 +0200

    Add Ticket.

---
+++ vue-app/src/shared/classes/Ticket.ts
@@ -0,0 +1,59 @@
+import type { ITicket } from '../interfaces/ITicket'
+
+export class Ticket implements ITicket {
+  TicketId: number
+  Name: string
+  Price: number
+  MaxPerTransaction: number
+  MinPerTransaction: number
+  MaxPerCustomer: number
+  MinPerCustomer: number
+  TotalTickets: number
+  Description: string
+  LongDescription: string
+  AvailableTickets: number
+  TicketEventId: number
+  TicketEventName: string
+  DisplayOrder: number
+  TagType: string
+  TicketTagValues: string[]
+  AvailableOn: number
+  Status: string
+  TicketType: string
+  MappingId: number
+  ItemCode: string
+  Category: string
+  MaxSales: number
+  RemainingCapacity: number
+  EventMaxCapacity: number
+  EventRemainingCapacity: number
+
+  constructor() {
+    this.TicketId = 0
+    this.Name = ''
+    this.Price = 0
+    this.MaxPerTransaction = 0
+    this.MinPerTransaction = 0
+    this.MaxPerCustomer = 0
+    this.MinPerCustomer = 0
+    this.TotalTickets = 0
+    this.Description = ''
+    this.LongDescription = ''
+    this.AvailableTickets = 0
+    this.TicketEventId = 0
+    this.TicketEventName = ''
+    this.DisplayOrder = 0
+    this.TagType = ''
+    this.TicketTagValues = []
+    this.AvailableOn = 0
+    this.Status = ''
+    this.TicketType = ''
+    this.MappingId = 0
+    this.ItemCode = ''
+    this.Category = ''
+    this.MaxSales = 0
+    this.RemainingCapacity = 0
+    this.EventMaxCapacity = 0
+    this.EventRemainingCapacity = 0
+  }
+}
