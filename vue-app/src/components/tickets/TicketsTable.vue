7db3f29adf452debafacce03b3a3edf108c64ecd
Sandro Räss <s.raess@me.com>
2024-05-24 16:01:42 +0200

    Wrap table-row in template to remove warning.

--- vue-app/src/components/tickets/TicketsTable.vue
+++ vue-app/src/components/tickets/TicketsTable.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { TicketHelper } from '@/helpers/TicketHelper'
 import type { ITicket } from '@/shared/interfaces/ITicket'
-import { on } from 'events'
 import { computed, onMounted, ref } from 'vue'
 const ticketHelper = new TicketHelper()
 const props = defineProps({
@@ -37,24 +36,26 @@ onMounted(() => {
 <template>
   <v-table class="border">
     <tbody>
-      <tr v-for="group in ticketsGrouped" :key="group[1][0].TicketId" v-if="!collsapsed">
-        <td>{{ group[1].length }} x</td>
-        <td>{{ group[1][0].Name }}</td>
-        <td>{{ group[1][0].Price }}</td>
-        <td class="border-e border-s">
-          {{ group[1].length * group[1][0].Price }}
-        </td>
-        <td v-if="showButtons">
-          <div class="d-flex">
-            <v-btn variant="text" @click="addTicket(group[1][0])" icon>
-              <v-icon class="text-gray"> mdi-plus </v-icon>
-            </v-btn>
-            <v-btn variant="text" @click="removeTicket(group[1][0])" icon>
-              <v-icon class="text-gray"> mdi-minus </v-icon>
-            </v-btn>
-          </div>
-        </td>
-      </tr>
+      <template v-if="!collsapsed">
+        <tr v-for="group in ticketsGrouped" :key="group[1][0].TicketId">
+          <td>{{ group[1].length }} x</td>
+          <td>{{ group[1][0].Name }}</td>
+          <td>{{ group[1][0].Price }}</td>
+          <td class="border-e border-s">
+            {{ group[1].length * group[1][0].Price }}
+          </td>
+          <td v-if="showButtons">
+            <div class="d-flex">
+              <v-btn variant="text" @click="addTicket(group[1][0])" icon>
+                <v-icon class="text-gray"> mdi-plus </v-icon>
+              </v-btn>
+              <v-btn variant="text" @click="removeTicket(group[1][0])" icon>
+                <v-icon class="text-gray"> mdi-minus </v-icon>
+              </v-btn>
+            </div>
+          </td>
+        </tr>
+      </template>
       <tr class="bg-lightgray">
         <td>
           <v-btn v-if="collapsible" @click="collapse()" variant="text" icon>
