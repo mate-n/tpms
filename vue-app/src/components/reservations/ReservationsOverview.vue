9441baf39baf7aa0345038e6091ab9790a216178
Sandro Räss <s.raess@me.com>
2024-05-28 17:38:14 +0200

    Add ReservationsOverview.

---
+++ vue-app/src/components/reservations/ReservationsOverview.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-toolbar
+    class="bg-primary text-white d-flex justify-space-between"
+    :title="$t('reservation.plural')"
+    app
+  >
+    <v-btn class="bg-white me-2">
+      <v-icon icon="mdi-select-all"></v-icon>
+
+      BULK
+    </v-btn>
+    <v-btn class="bg-white me-2">
+      <v-icon icon="mdi-filter-variant"></v-icon>
+
+      Filter
+    </v-btn>
+    <v-menu>
+      <template v-slot:activator="{ props, isActive }">
+        <v-btn class="bg-white" v-bind="props">
+          <v-icon>mdi-printer-outline</v-icon>
+          PRINT
+          <v-icon v-if="isActive" icon="mdi-chevron-up"></v-icon>
+          <v-icon v-if="!isActive" icon="mdi-chevron-down"></v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title class="d-flex justify-space-between">
+            New <v-icon>mdi-playlist-check</v-icon>
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item disabled class="bg-lightgray">
+          <v-list-item-title class="d-flex justify-space-between">
+            Existing <v-icon>mdi-playlist-plus</v-icon>
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-toolbar>
+  <div class="d-flex bg-lightgray"></div>
+</template>
