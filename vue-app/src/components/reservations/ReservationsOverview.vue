e0aaea4e5bc90a8d60dea75992e299a1dc2a7a07
Metaphysiker <s.raess@me.com>
2024-05-30 12:07:22 +0200

    Update ReservationsOverview to use ReservationsTable and fields for query-input.

--- vue-app/src/components/reservations/ReservationsOverview.vue
+++ vue-app/src/components/reservations/ReservationsOverview.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { inject, onMounted, ref, type Ref } from 'vue'
+import ReservationsTable from './ReservationsTable.vue'
+import type { IReservation } from '@/shared/interfaces/IReservation'
+import type { AxiosStatic } from 'axios'
+import { ReservationService } from '@/services/ReservationService'
+const axios: AxiosStatic | undefined = inject('axios')
+const reservationService = new ReservationService(axios)
+const reservations: Ref<IReservation[]> = ref([])
+onMounted(() => {
+  reservationService.getAll().then((data) => {
+    reservations.value = data
+  })
+})
+</script>
 
 <template>
   <v-toolbar
@@ -39,5 +53,67 @@
       </v-list>
     </v-menu>
   </v-toolbar>
-  <div class="d-flex bg-lightgray"></div>
+  <v-container fluid class="bg-lightgray">
+    <v-row class="d-flex align-center">
+      <v-col>
+        <v-autocomplete
+          label="Search"
+          item-title="name"
+          item-value="id"
+          variant="underlined"
+        ></v-autocomplete>
+      </v-col>
+      <v-col>
+        <v-menu :close-on-content-click="false">
+          <template v-slot:activator="{ props }">
+            <v-text-field
+              label="From"
+              append-inner-icon="mdi-calendar"
+              class="required-input"
+              variant="underlined"
+              v-bind="props"
+            ></v-text-field>
+          </template>
+          <v-card>
+            <v-date-picker :hide-header="true"> </v-date-picker>
+          </v-card>
+        </v-menu>
+      </v-col>
+      <v-col>
+        <v-menu :close-on-content-click="false">
+          <template v-slot:activator="{ props }">
+            <v-text-field
+              label="To"
+              append-inner-icon="mdi-calendar"
+              variant="underlined"
+              class="required-input"
+              v-bind="props"
+            ></v-text-field>
+          </template>
+          <v-card>
+            <v-date-picker :hide-header="true"></v-date-picker>
+          </v-card>
+        </v-menu>
+      </v-col>
+      <v-col>
+        <v-autocomplete
+          label="Type"
+          item-title="name"
+          item-value="id"
+          variant="underlined"
+        ></v-autocomplete>
+      </v-col>
+      <v-col class="d-flex justify-space-between">
+        <v-btn class="primary-button mr-3 w-100">SEARCH</v-btn>
+      </v-col>
+    </v-row>
+    <div>
+      <v-checkbox label="Confidential Rate" :hide-details="true" density="compact"></v-checkbox>
+      <v-checkbox label="Day Use" :hide-details="true" density="compact"></v-checkbox>
+    </div>
+  </v-container>
+
+  <v-container fluid class="bg-white">
+    <ReservationsTable :reservations="reservations" />
+  </v-container>
 </template>
