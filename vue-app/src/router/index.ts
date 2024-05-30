39aa954be4bb682d3913ae5016e9342e448cf7f9
Metaphysiker <s.raess@me.com>
2024-05-30 12:06:29 +0200

    Add route to edit-reservation-view.

--- vue-app/src/router/index.ts
+++ vue-app/src/router/index.ts
@@ -4,6 +4,7 @@ import NewProfileView from '@/views/NewProfileView.vue'
 import ProfileSearchView from '@/views/ProfileSearchView.vue'
 import ApiTestView from '@/views/ApiTestView.vue'
 import ReservationsView from '@/views/ReservationsView.vue'
+import EditReservationView from '@/views/EditReservationView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -37,6 +38,12 @@ const router = createRouter({
       path: '/reservations',
       name: 'reservations',
       component: ReservationsView
+    },
+    {
+      path: '/reservations/:reservationId',
+      name: 'edit reservation',
+      component: EditReservationView,
+      props: true
     },
     {
       path: '/about',
