9f4803df1acfc98ab5587675f1d7fefe29b44f73
Metaphysiker <s.raess@me.com>
2024-05-31 20:11:35 +0200

    Add ProfilesView.

---
+++ vue-app/src/views/ProfilesView.vue
@@ -0,0 +1,11 @@
+<script setup="ts">
+import ProfileSearch from '@/components/profiles/ProfileSearch.vue'
+</script>
+
+<template>
+  <v-toolbar color="primary" title="Profiles"></v-toolbar>
+  <ProfileSearch
+    @close="closeProfileDialog()"
+    @profile-selected="(profile) => profileSelected(profile)"
+  ></ProfileSearch>
+</template>
