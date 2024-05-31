78a61df196bd2f5d70823c5a4663b59d484085d4
Metaphysiker <s.raess@me.com>
2024-05-31 20:10:46 +0200

    Add EditItineraryReservation.

---
+++ vue-app/src/components/reservations/EditItineraryReservation.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue'
+import { CrudOperations } from '@/enums/CrudOperations'
+import { CloneHelper } from '@/helpers/CloneHelper'
+import { useFullscreen } from '@/composables/Fullscreen'
+import { Profile } from '@/shared/classes/Profile'
+import type { IProfile } from '@/shared/interfaces/profiles/IProfile'
+import ReservationForm from './ReservationForm.vue'
+import type { IReservation } from '@/shared/interfaces/IReservation'
+import { Reservation } from '@/shared/classes/Reservation'
+
+const cloneHelper = new CloneHelper()
+const emit = defineEmits(['close', 'update'])
+const close = () => emit('close')
+const props = defineProps({
+  reservationInput: { type: Object as () => IReservation, required: true }
+})
+const reservationToBeEdited = ref<IReservation>(new Reservation())
+const update = (profile: IProfile) => {
+  emit('update', profile)
+}
+
+onMounted(() => {
+  reservationToBeEdited.value = cloneHelper.clone(props.reservationInput)
+})
+
+watch(props, (newInput) => {
+  reservationToBeEdited.value = cloneHelper.clone(newInput.reservationInput)
+})
+</script>
+<template>
+  <ReservationForm
+    :reservation-input="reservationToBeEdited"
+    @save="(profile) => update(profile)"
+    :crud-operation="CrudOperations.Update"
+  ></ReservationForm>
+</template>
