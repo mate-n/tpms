4d46977e865a558eb4659d123bf0d284d7642faa
Sandro Räss <s.raess@me.com>
2024-06-01 06:11:36 +0200

    Add IEntityWithIdentity to ItineraryReservation.

--- vue-app/src/shared/interfaces/IItineraryReservation.ts
+++ vue-app/src/shared/interfaces/IItineraryReservation.ts
@@ -1,5 +1,6 @@
+import type { IEntityWithIdentity } from './IEntityWithIdentity'
 import type { IReservation } from './IReservation'
 
-export interface IItineraryReservation {
+export interface IItineraryReservation extends IEntityWithIdentity {
   reservations: IReservation[]
 }
