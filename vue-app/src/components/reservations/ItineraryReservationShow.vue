e64e8bac0e6e78f4b831166e80b8c42fa557bf55
Sandro Räss <s.raess@me.com>
2024-06-01 06:13:16 +0200

    Add ItineraryReservationShow.

---
+++ vue-app/src/components/reservations/ItineraryReservationShow.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { IItineraryReservation } from '@/shared/interfaces/IItineraryReservation'
+
+const props = defineProps<{
+  itineraryReservation: IItineraryReservation
+}>()
+</script>
+
+<template>
+  <v-card class="mb-3">
+    <v-card-title>
+      {{ props.itineraryReservation.id }}
+    </v-card-title>
+    <v-card-text>
+      <v-btn> EDIT </v-btn>
+    </v-card-text>
+  </v-card>
+</template>
