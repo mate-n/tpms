aca7a8634488aac7a91d47805540b825b91678c6
Sandro Räss <s.raess@me.com>
2024-06-01 13:38:40 +0200

    Add route for itinerary-reservation.

--- vue-app/src/router/index.ts
+++ vue-app/src/router/index.ts
@@ -7,6 +7,7 @@ import ReservationsView from '@/views/ReservationsView.vue'
 import EditReservationView from '@/views/EditReservationView.vue'
 import DashboardView from '@/views/DashboardView.vue'
 import ItineraryReservationsView from '@/views/ItineraryReservationsView.vue'
+import EditItineraryReservationView from '@/views/EditItineraryReservationView.vue'
 import ProfilesView from '@/views/ProfilesView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -50,6 +51,12 @@ const router = createRouter({
       path: '/reservations/:reservationId',
       name: 'edit reservation',
       component: EditReservationView,
+      props: true
+    },
+    {
+      path: '/itinerary-reservations/:itineraryReservationId',
+      name: 'itinerary reservation',
+      component: EditItineraryReservationView,
       props: true
     },
     {
