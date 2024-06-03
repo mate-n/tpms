d6ea818da1b49aebce0be564ceee8a58e39f99d5
Sandro Räss <s.raess@me.com>
2024-06-03 10:58:03 +0200

    Add startDate, endDate, startPropertyName and endPropertyName to ItineraryReservation and IItineraryReservation.

--- vue-app/src/shared/interfaces/IItineraryReservation.ts
+++ vue-app/src/shared/interfaces/IItineraryReservation.ts
@@ -3,4 +3,8 @@ import type { IReservation } from './IReservation'
 
 export interface IItineraryReservation extends IEntityWithIdentity {
   reservations: IReservation[]
+  startDate: Date
+  endDate: Date
+  startPropertyName: string
+  endPropertyName: string
 }
