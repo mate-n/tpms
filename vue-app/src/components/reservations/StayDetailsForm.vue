c47fa94bbc161885f8ab2ef73162fb868f8f4f76
Sandro Räss <s.raess@me.com>
2024-06-04 22:57:33 +0200

    Add StayDetailsForm.

---
+++ vue-app/src/components/reservations/StayDetailsForm.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { IReservation } from '@/shared/interfaces/IReservation'
+import DateSelecter from '../dates/DateSelecter.vue'
+const reservationToBeEdited = defineModel({ required: true, type: Object as () => IReservation })
+</script>
+<template>
+  <div class="profiles-card">
+    <v-toolbar class="profiles-card-toolbar">
+      <v-toolbar-title><span class="text-primary">Stay Details</span></v-toolbar-title>
+    </v-toolbar>
+    <v-divider class="profiles-card-divider"></v-divider>
+    <v-container>
+      <v-row>
+        <v-col>
+          <DateSelecter v-model="reservationToBeEdited.arrivalDate" label="Arrival"></DateSelecter>
+        </v-col>
+        <v-col>
+          <DateSelecter v-model="reservationToBeEdited.arrivalDate" label="Optional"></DateSelecter>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col> </v-col>
+        <v-col>
+          <v-text-field
+            label="Guests per room"
+            v-model="reservationToBeEdited.numberOfGuestsPerRoom"
+            :error-messages="
+              reservationToBeEdited.errors && reservationToBeEdited.errors['numberOfGuestsPerRoom']
+            "
+            variant="underlined"
+            type="number"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
