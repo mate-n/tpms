1303f40e590a4967900bf3579836835e0718566e
Sandro Räss <s.raess@me.com>
2024-06-07 08:06:05 +0200

    Add LoginCard.

---
+++ vue-app/src/components/authentication/LoginCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { AuthenticationHelper } from '@/authentication/AuthenticationHelper'
+import AuthenticationService from '@/services/AuthenticationService'
+import type { IAccessToken } from '@/shared/interfaces/IAccessToken'
+import type { AxiosStatic } from 'axios'
+import { inject, ref, type Ref } from 'vue'
+const authenticationHelper = new AuthenticationHelper()
+const axios: AxiosStatic | undefined = inject('axios')
+const authentificationService = new AuthenticationService(axios)
+const clickOnLogin = async () => {
+  authentificationService.login(login.value, password.value).then((response: IAccessToken) => {
+    authenticationHelper.setAccessToken(response.access_token)
+    if (axios) {
+      axios.defaults.headers.common['Authorization'] = `Bearer ${response.access_token}`
+    }
+  })
+}
+
+const login = ref('')
+const password = ref('')
+</script>
+<template>
+  <v-card>
+    <v-card-text>
+      <v-text-field label="Login" v-model="login" variant="underlined"></v-text-field>
+      <v-text-field
+        label="Password"
+        v-model="password"
+        variant="underlined"
+        type="Password"
+      ></v-text-field>
+      <v-btn @click="clickOnLogin()">Login</v-btn>
+    </v-card-text>
+  </v-card>
+</template>
