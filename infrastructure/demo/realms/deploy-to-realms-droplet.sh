28f734294548bb63b3d306040532fae342dd86ae
Sandro Räss <s.raess@me.com>
2024-06-07 13:31:57 +0200

    Add deploy-to-realms-droplet.sh.

---
+++ infrastructure/demo/realms/deploy-to-realms-droplet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker build . -t realms --build-context root=../../../ --build-arg VITE_API_BASE_URL=http://138.68.84.157:3001/
+
+ssh tpms-user@138.68.84.157 << EOF
+    docker stop realms
+    docker rm realms
+EOF
+
+docker save realms | bzip2 | ssh tpms-user@138.68.84.157 docker load
+
+ssh tpms-user@138.68.84.157 << EOF
+    docker run -d -p 3000:3000 -p 3001:3001 --name realms --rm realms
+EOF
