40aa00710379c82bb34c790bcf9c9cd6eac5ffe7
Metaphysiker <s.raess@me.com>
2024-06-07 17:50:56 +0200

    Update main.ts to use environment-variable for port if it exists.

--- nest-app/src/main.ts
+++ nest-app/src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 declare const module: any;
+const PORT = process.env.BACKEND_PORT || 3001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
-  await app.listen(3001);
+  await app.listen(PORT);
 
   if (module.hot) {
     module.hot.accept();
