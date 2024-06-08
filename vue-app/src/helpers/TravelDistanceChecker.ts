55bb77bbbf42b5211eb53c1e58c24b02e19ed3ca
Metaphysiker <s.raess@me.com>
2024-06-08 15:06:12 +0200

    Update name for camps in TravelDistanceChecker.

--- vue-app/src/helpers/TravelDistanceChecker.ts
+++ vue-app/src/helpers/TravelDistanceChecker.ts
@@ -1,6 +1,6 @@
 export class TravelDistanceChecker {
   isDistanceIsPossibleToTravelWithinADay(originCamp: string, targetCamp: string): boolean {
-    if (originCamp === 'Twee Rivieren' && targetCamp === 'Gharagab') {
+    if (originCamp === 'Twee Rivieren' && targetCamp === 'Gharagab Wilderness Camp') {
       return false
     }
     return true
