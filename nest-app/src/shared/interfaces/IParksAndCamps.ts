cc21b552314fdbc65e03b45c6435382726e22f95
Metaphysiker <s.raess@me.com>
2024-06-08 15:14:15 +0200

    Add IParksAndCamps.

---
+++ nest-app/src/shared/interfaces/IParksAndCamps.ts
@@ -0,0 +1,16 @@
+import type { ICamp } from './ICamp';
+
+export interface IParksAndCamps {
+  'Kruger National Park': KrugerNationalPark;
+  'Kgalagadi Transfrontier Park': KgalagadiTransfrontierPark;
+}
+
+export interface KrugerNationalPark {
+  parknid: string;
+  camps: ICamp[];
+}
+
+export interface KgalagadiTransfrontierPark {
+  parknid: string;
+  camps: ICamp[];
+}
