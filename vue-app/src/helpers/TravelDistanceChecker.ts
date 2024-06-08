f9d69dced9ba3f0a43921a3ec1929e749e7bd3a9
Sandro Räss <s.raess@me.com>
2024-06-08 19:18:40 +0200

    Update TravelDistanceChecker to use correct names for camps.

--- vue-app/src/helpers/TravelDistanceChecker.ts
+++ vue-app/src/helpers/TravelDistanceChecker.ts
@@ -1,6 +1,6 @@
 export class TravelDistanceChecker {
   isDistanceIsPossibleToTravelWithinADay(originCamp: string, targetCamp: string): boolean {
-    if (originCamp === 'Twee Rivieren' && targetCamp === 'Gharagab Wilderness Camp') {
+    if (originCamp === 'Twee Riveren' && targetCamp === 'Gharagab Wilderness Camp') {
       return false
     }
     return true
