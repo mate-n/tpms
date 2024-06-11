baed3a8524584c55b03db1bfcf6a17aefdd42e4f
Metaphysiker <s.raess@me.com>
2024-06-11 18:14:12 +0200

    Update TicketsTable to format date of ticket.

--- vue-app/src/components/tickets/TicketsTable.vue
+++ vue-app/src/components/tickets/TicketsTable.vue
@@ -55,7 +55,9 @@ onMounted(() => {
         <tr v-for="group in ticketsGrouped" :key="group[1][0].TicketId">
           <td>{{ group[1].length }} x</td>
           <td>{{ group[1][0].Name }}</td>
-          <td v-if="showDate">{{ dateFormatter.dddotmmdotyyyy(group[1][0].Date) }}</td>
+          <td v-if="showDate">
+            {{ dateFormatter.dddotmmdotyyyy(group[1][0].Date) }}
+          </td>
           <td>{{ group[1][0].Price }}</td>
           <td>
             {{ (group[1].length * group[1][0].Price).toFixed(2) }}
