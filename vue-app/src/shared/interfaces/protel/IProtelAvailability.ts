b9915ff3f4f5e2c1ec6c6ff798611dc603208ca3
Sandro Räss <s.raess@me.com>
2024-06-11 13:27:10 +0200

    Add IProtelAvailability.

---
+++ vue-app/src/shared/interfaces/protel/IProtelAvailability.ts
@@ -0,0 +1,28 @@
+export interface IProtelAvailability {
+  id: string
+  transaction_id: string
+  unique_message_id: string
+  request_id: string
+  date_time: string
+  pms_type: string
+  property_code: string
+  type: string
+  room_type_code: string
+  default_occupancy: string
+  max_occupancy: string
+  availability_start: string
+  availability_end: string
+  availability_count: string
+  park_name: string
+  property_name: string
+  room_type_name: string
+  features: string
+  rates_data: IRatesData[]
+}
+
+interface IRatesData {
+  rates_code: string
+  occupancy: string
+  room_rate: string
+  add_on_child1: string
+}
