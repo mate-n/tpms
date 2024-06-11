f9268257f6758eaf2d1b6f290bd011314f15e3d3
Sandro Räss <s.raess@me.com>
2024-06-11 14:01:17 +0200

    Add ProtelAvailabilitesModule to app.module.

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -1,19 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProfilesModule } from './profiles/v1/profiles.module';
-import { PropertiesModule } from './properties/v1/properties.module';
-import { DropdownsController } from './dropdowns/v1/dropdowns.controller';
-import { RoomsController } from './rooms/v1/rooms.controller';
-import { ProfileAddressesController } from './profile-addresses/v1/profile-addresses.controller';
-import { ProfileAddressesService } from './profile-addresses/v1/profile-addresses.service';
-import { ProfileCommunicationsController } from './profile-communications/v1/profile-communications.controller';
-import { StationeriesModule } from './stationeries/v1/stationeries.module';
-import { ReservationsModule } from './reservations/v1/reservations.module';
-import { ItineraryReservationsModule } from './itinerary-reservations/v1/itinerary-reservations.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { ProtelCampsModule } from './protel-camps/v1/protel-camps.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ProfilesModule } from './profiles/v1/profiles.module'
+import { PropertiesModule } from './properties/v1/properties.module'
+import { DropdownsController } from './dropdowns/v1/dropdowns.controller'
+import { RoomsController } from './rooms/v1/rooms.controller'
+import { ProfileAddressesController } from './profile-addresses/v1/profile-addresses.controller'
+import { ProfileAddressesService } from './profile-addresses/v1/profile-addresses.service'
+import { ProfileCommunicationsController } from './profile-communications/v1/profile-communications.controller'
+import { StationeriesModule } from './stationeries/v1/stationeries.module'
+import { ReservationsModule } from './reservations/v1/reservations.module'
+import { ItineraryReservationsModule } from './itinerary-reservations/v1/itinerary-reservations.module'
+import { AuthModule } from './auth/auth.module'
+import { UsersModule } from './users/users.module'
+import { ProtelCampsModule } from './protel-camps/v1/protel-camps.module'
+import { ProtelAvailabilitiesModule } from './protel-availabilities/v1/protel-availabilities.module'
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { ProtelCampsModule } from './protel-camps/v1/protel-camps.module';
     AuthModule,
     UsersModule,
     ProtelCampsModule,
+    ProtelAvailabilitiesModule,
   ],
   controllers: [
     AppController,
