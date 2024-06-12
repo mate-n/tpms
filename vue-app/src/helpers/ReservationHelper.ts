940798190760723fb52dc21f0664f03e5f569507
Metaphysiker <s.raess@me.com>
2024-06-12 09:22:05 +0200

    Add getNumberOfNights, calculateTotalRate, calculateTotalReservationPrice and getRoomRate to ReservationHelper.

--- vue-app/src/helpers/ReservationHelper.ts
+++ vue-app/src/helpers/ReservationHelper.ts
@@ -1,7 +1,9 @@
 import type { IReservation } from '@/shared/interfaces/IReservation'
 import { TicketHelper } from './TicketHelper'
+import { DateHelper } from './DateHelper'
 
 export class ReservationHelper {
+  dateHelper = new DateHelper()
   ticketHelper = new TicketHelper()
   isReservationFirstOrLastOfArray(reservation: IReservation, reservations: IReservation[]) {
     const reservationWithLowestOrderIndex = reservations.reduce(
@@ -17,10 +19,45 @@ export class ReservationHelper {
     return false
   }
 
+  getNumberOfNights(reservation: IReservation): number {
+    let numberOfNights = 0
+    numberOfNights = this.dateHelper.calculateNightsBetweenDates(
+      reservation.arrivalDate,
+      reservation.departureDate
+    )
+    return numberOfNights
+  }
+
+  calculateTotalRate(reservation: IReservation): number {
+    const numberOfNights = this.getNumberOfNights(reservation)
+    let roomRate = 0
+    if (reservation.selectedProtelAvailability) {
+      roomRate = parseFloat(reservation.selectedProtelAvailability.rates_data[0].room_rate)
+    }
+
+    return roomRate * numberOfNights
+  }
+
+  calculateTotalReservationPrice(reservation: IReservation): number {
+    let totalReservationPrice = 0
+    totalReservationPrice += this.calculateTotalRate(reservation)
+    totalReservationPrice += this.ticketHelper.getTotalPrice(reservation.tickets)
+    return totalReservationPrice
+  }
+
+  getRoomRate(reservation: IReservation): number {
+    let roomRate = 0
+    if (reservation.selectedProtelAvailability) {
+      roomRate = parseFloat(reservation.selectedProtelAvailability.rates_data[0].room_rate)
+    }
+    return roomRate
+  }
+
   getTotalPrice(reservations: IReservation[]): number {
     let total = 0
     for (const reservation of reservations) {
-      total += reservation.totalRate + this.ticketHelper.getTotalPrice(reservation.tickets)
+      const roomRate = this.calculateTotalRate(reservation)
+      total += roomRate + this.ticketHelper.getTotalPrice(reservation.tickets)
     }
     return total
   }
