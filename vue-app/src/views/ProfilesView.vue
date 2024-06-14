016caaeb0374c21b1af437617dca30fce11130d7
Metaphysiker <s.raess@me.com>
2024-06-14 19:37:39 +0200

    Update ProfilesView to have profileSearch and to create Profile from ProfileSearch for NewProfile.

--- vue-app/src/views/ProfilesView.vue
+++ vue-app/src/views/ProfilesView.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import ProfileSearch from '@/components/profiles/ProfileSearch.vue'
 import NewProfile from '@/components/profiles/NewProfile.vue'
-import { ref } from 'vue'
+import { ref, type Ref } from 'vue'
 import { Profile } from '@/shared/classes/Profile'
+import { ProfileSearchHelper } from '@/helpers/ProfileSearchHelper'
+import type { IProfileSearch } from '@/shared/interfaces/profiles/IProfileSearch'
+import { ProfileSearch as ProfileSearchClass } from '@/shared/classes/ProfileSearch'
+const profileSearchHelper = new ProfileSearchHelper()
 const newProfileDialog = ref(false)
-const profileFromInputFields = ref(new Profile())
+const profileForNewProfile = ref(new Profile())
+const profileSearch: Ref<IProfileSearch> = ref(new ProfileSearchClass())
 const clickOnAdd = () => {
+  profileForNewProfile.value = profileSearchHelper.convertToProfile(profileSearch.value)
   newProfileDialog.value = true
 }
 </script>
@@ -16,11 +22,11 @@ const clickOnAdd = () => {
       ><v-icon>mdi-plus</v-icon>Add</v-btn
     >
   </v-toolbar>
-  <ProfileSearch @close="newProfileDialog = false"></ProfileSearch>
+  <ProfileSearch @close="newProfileDialog = false" v-model="profileSearch"></ProfileSearch>
   <v-dialog v-model="newProfileDialog" fullscreen scrollable>
     <v-card>
       <NewProfile
-        :profile-input="profileFromInputFields"
+        :profile-input="profileForNewProfile"
         @close="newProfileDialog = false"
       ></NewProfile>
     </v-card>
