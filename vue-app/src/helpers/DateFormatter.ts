4ce7da6b2406204a0a79e88ce496640946e238b9
Metaphysiker <s.raess@me.com>
2024-06-18 13:57:43 +0200

    Add dddotmm to DateFormatter.

--- vue-app/src/helpers/DateFormatter.ts
+++ vue-app/src/helpers/DateFormatter.ts
@@ -26,6 +26,11 @@ export class DateFormatter {
     )
   }
 
+  dddotmm(dateInput: Date) {
+    const date = new Date(dateInput)
+    return this.addZeroPadding(date.getDate()) + '.' + this.addZeroPadding(date.getMonth() + 1)
+  }
+
   addZeroPadding(number: number) {
     return ('0' + number).slice(-2)
   }
