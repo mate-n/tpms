8214dcb03f34e28be0d23e1a560151660d0b3e08
Metaphysiker <s.raess@me.com>
2024-06-18 13:58:22 +0200

    Update AvailabilityService to convert availabilites before resolving.

--- vue-app/src/services/protel/AvailabilityService.ts
+++ vue-app/src/services/protel/AvailabilityService.ts
@@ -1,10 +1,12 @@
 import { AxiosInstanceFactory } from '@/factories/AxiosInstanceFactory'
+import { AvailabilityHelper } from '@/helpers/AvailabilityHelper'
 import type { IService } from '@/interfaces/IService'
 import type { IProtelAvailability } from '@/shared/interfaces/protel/IProtelAvailability'
 import type { IProtelAvailabilityPostBody } from '@/shared/interfaces/protel/IProtelAvailabilityPostBody'
 import { type AxiosStatic } from 'axios'
 
 export class AvailabilityService implements IService {
+  availabilityHelper: AvailabilityHelper = new AvailabilityHelper()
   axiosInstance: AxiosStatic
   constructor(axios: AxiosStatic | undefined) {
     this.axiosInstance = AxiosInstanceFactory.createAxiosInstance(axios)
@@ -15,7 +17,15 @@ export class AvailabilityService implements IService {
       this.axiosInstance
         .post('v1/protel-availabilities/search', protelAvailabilityPostBody)
         .then((response: any) => {
-          resolve(response.data)
+          const availabilities = response.data
+
+          const convertedAvailabilities = availabilities.map(
+            (availability: IProtelAvailability) => {
+              this.availabilityHelper.convertToAvailability(availability)
+              return availability
+            }
+          )
+          resolve(convertedAvailabilities)
         })
         .catch((e: any) => {
           reject(e)
