d9dee7ae771866d9c40dac1d9c38e15d8e189919
Sandro Räss <s.raess@me.com>
2024-06-19 20:09:42 +0200

    Update example-test to look for name tpms.

--- vue-app/cypress/e2e/example.cy.ts
+++ vue-app/cypress/e2e/example.cy.ts
@@ -3,6 +3,6 @@
 describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('tpms-frontend')
+    cy.contains('tpms', { matchCase: false })
   })
 })
