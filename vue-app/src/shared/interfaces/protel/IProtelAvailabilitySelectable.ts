073c36d42a35aafb3279ff818ff87fcfa07b1271
Metaphysiker <s.raess@me.com>
2024-06-19 11:33:38 +0200

    Add element to IProtelAvailabilitySelectable.

--- vue-app/src/shared/interfaces/protel/IProtelAvailabilitySelectable.ts
+++ vue-app/src/shared/interfaces/protel/IProtelAvailabilitySelectable.ts
@@ -3,4 +3,5 @@ import type { IProtelAvailability } from './IProtelAvailability'
 export interface IProtelAvailabilitySelectable {
   selected: boolean
   availability: IProtelAvailability
+  element?: any
 }
