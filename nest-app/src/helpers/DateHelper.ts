71996b7833fc7bdebf27c72abd09ad530ecceef4
Metaphysiker <s.raess@me.com>
2024-06-21 15:41:48 +0200

    Update CalculateNights in DateHelper to calculate nights correctly.

--- nest-app/src/helpers/DateHelper.ts
+++ nest-app/src/helpers/DateHelper.ts
@@ -3,7 +3,7 @@ export class DateHelper {
     const arrival = new Date(arrivalDate)
     const departure = new Date(departureDate)
     const timeDifference = departure.getTime() - arrival.getTime()
-    return Math.ceil(timeDifference / (1000 * 3600 * 24))
+    return Math.ceil(timeDifference / (1000 * 3600 * 24)) - 1
   }
 
   getDateString(date: Date): string {
