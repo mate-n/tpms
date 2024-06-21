faa8354f1a4cfbf81ca52bfcc0f886076456fd52
Metaphysiker <s.raess@me.com>
2024-06-21 14:51:20 +0200

    Update RoomDetailsCard to display mock-data.

--- vue-app/src/components/rooms/RoomDetailsCard.vue
+++ vue-app/src/components/rooms/RoomDetailsCard.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
-import type { IProtelAvailability } from '@/shared/interfaces/protel/IProtelAvailability'
-
 const emits = defineEmits(['close'])
-defineProps({
-  protelAvailability: { type: Object as () => IProtelAvailability, required: true }
-})
 </script>
 <template>
   <div class="standard-card bg-lightgray">
@@ -26,20 +21,32 @@ defineProps({
           <v-carousel-item src="https://picsum.photos/800/600?sig=3" cover></v-carousel-item>
         </v-carousel>
         <v-card-text>
-          <h1>{{ protelAvailability.property_name }}</h1>
-          <h2 class="mt-3">{{ protelAvailability.room_type_name }}</h2>
+          <h1>Kruger National Park</h1>
+          <h2 class="mt-3">Family Chalet FCH6</h2>
           <v-divider class="my-2"></v-divider>
           <div>
             <h3 class="font-weight-bold mb-2">What this place offers</h3>
-            <div class="ms-4" v-html="protelAvailability.features"></div>
+            <div class="ms-4">
+              <ul>
+                <li>2 bedrooms, each with 2 single beds</li>
+                <li>2 single beds in lounge</li>
+                <li>Air-conditioned</li>
+                <li>Fully Equipped</li>
+                <li>Internal roads not suitable for sedan vehicles</li>
+                <li>
+                  Kitchen includes 2 plate stove and fridge/freezer No additional persons allowed No
+                </li>
+                <li>smoking WC and shower</li>
+              </ul>
+            </div>
           </div>
           <div class="mt-3">
             <h3 class="font-weight-bold mb-2">Room Rate</h3>
-            {{ protelAvailability.rates_data[0].room_rate }}
+            2,322.95
           </div>
           <div class="mt-3">
             <h3 class="font-weight-bold mb-2">Free Rooms</h3>
-            {{ protelAvailability.availability_count }}
+            4
           </div>
         </v-card-text>
       </v-card>
