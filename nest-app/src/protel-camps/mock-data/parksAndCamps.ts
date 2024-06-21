e1e9fa18c66237c9dc7206b3c5c227c6f55e93dc
Metaphysiker <s.raess@me.com>
2024-06-21 14:29:53 +0200

    Add parksAndCamps.

---
+++ nest-app/src/protel-camps/mock-data/parksAndCamps.ts
@@ -0,0 +1,46 @@
+const parksAndCamps = {
+  'Kruger National Park': {
+    parkid: '1',
+    camps: [
+      {
+        campid: '3599',
+        campname: 'Skukuza Rest Camp',
+      },
+      {
+        campid: '3600',
+        campname: 'Lower Sabie Rest Camp',
+      },
+    ],
+  },
+  'Kgalagadi Transfrontier Park': {
+    parkid: '2',
+    camps: [
+      {
+        campid: '19315',
+        campname: 'Mata Mata',
+      },
+      {
+        campid: '19317',
+        campname: 'Kieliekrankie Wilderness Camp',
+      },
+      {
+        campid: '19321',
+        campname: 'Gharagab Wilderness Camp',
+      },
+      {
+        campid: '19325',
+        campname: 'Twee Riveren',
+      },
+      {
+        campid: '19335',
+        campname: 'Grootkolk Wilderness Camp',
+      },
+      {
+        campid: '19337',
+        campname: 'Kalahari Tent Camp',
+      },
+    ],
+  },
+}
+
+export default parksAndCamps
