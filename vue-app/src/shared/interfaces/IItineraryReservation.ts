affe9eba53b722eb009b5f1a733c1f98fb5713f3
Metaphysiker <s.raess@me.com>
2024-06-25 18:35:13 +0200

    Add arrivalDate, departureDate, selectedRegions, selectedParks, selectedCamps to ItineraryReservation.

--- vue-app/src/shared/interfaces/IItineraryReservation.ts
+++ vue-app/src/shared/interfaces/IItineraryReservation.ts
@@ -1,10 +1,17 @@
 import type { IEntityWithIdentity } from './IEntityWithIdentity'
 import type { IReservation } from './IReservation'
+import type { IProtelCamp } from './protel/IProtelCamp'
+import type { IProtelPark } from './protel/IProtelPark'
+import type { IProtelRegion } from './protel/IProtelRegion'
 
 export interface IItineraryReservation extends IEntityWithIdentity {
   reservations: IReservation[]
-  startDate: Date
-  endDate: Date
   startPropertyName: string
   endPropertyName: string
+  arrivalDate: Date
+  departureDate: Date
+  selectedRegions: IProtelRegion[]
+  selectedParks: IProtelPark[]
+  selectedCamps: IProtelCamp[]
+  guestProfileID: number | undefined
 }
