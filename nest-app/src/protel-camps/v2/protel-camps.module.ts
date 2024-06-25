bdccadae4d336f47806d5a44f1e0dd9604d4696a
Sandro Räss <s.raess@me.com>
2024-06-25 14:01:05 +0200

    Add v2 for protel-camps.

---
+++ nest-app/src/protel-camps/v2/protel-camps.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common'
+import { ProtelCampsService } from './protel-camps.service'
+import { ProtelCampsController } from './protel-camps.controller'
+
+@Module({
+  controllers: [ProtelCampsController],
+  providers: [ProtelCampsService],
+})
+export class ProtelCampsModule {}
