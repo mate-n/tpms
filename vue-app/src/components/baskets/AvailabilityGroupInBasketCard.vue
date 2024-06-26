961285fb0d246ebd7bb7f61ce8e694edb12782f1
Sandro Räss <s.raess@me.com>
2024-06-26 06:05:52 +0200

    Add AvailabilityGroupInBasketCard.

---
+++ vue-app/src/components/baskets/AvailabilityGroupInBasketCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { AvailabilityGroupHelper } from '@/helpers/AvailabilityGroupHelper'
+import type { IProtelAvailabilityGroup } from '@/shared/interfaces/protel/IProtelAvailabilityGroup'
+import { onBeforeMount } from 'vue'
+const availabilityGroupHelper = new AvailabilityGroupHelper()
+const props = defineProps({
+  availabilityGroup: { type: Object as () => IProtelAvailabilityGroup, required: true }
+})
+onBeforeMount(() => {
+  availabilityGroupHelper.sortByDate(props.availabilityGroup)
+})
+</script>
+
+<template>
+  <div v-for="availability of availabilityGroup.availabilities" :key="availability.id">
+    <div>
+      <div>{{ availability.availability_count }}</div>
+      <div>{{ availability.id }}</div>
+    </div>
+  </div>
+</template>
