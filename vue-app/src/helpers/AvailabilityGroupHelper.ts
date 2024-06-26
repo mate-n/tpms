b47414cc52a9c9b21b49ee134ede8774216c38f7
Metaphysiker <s.raess@me.com>
2024-06-26 09:10:51 +0200

    Add calculateTotalRate, getDepartureDate and getArrivalDate to AvailabilityGroupHelper.

--- vue-app/src/helpers/AvailabilityGroupHelper.ts
+++ vue-app/src/helpers/AvailabilityGroupHelper.ts
@@ -1,7 +1,12 @@
 import type { IReservation } from '@/shared/interfaces/IReservation'
 import type { IProtelAvailabilityGroup } from '@/shared/interfaces/protel/IProtelAvailabilityGroup'
+import { DateHelper } from './DateHelper'
+import { RatesHelper } from './RatesHelper'
+import type { IGuestsPerRoom } from '@/shared/interfaces/IGuestsPerRoom'
 
 export class AvailabilityGroupHelper {
+  dateHelper = new DateHelper()
+  ratesHelper = new RatesHelper()
   sortByDate(availabilityGroup: IProtelAvailabilityGroup) {
     availabilityGroup.availabilities.sort((a, b) => {
       return new Date(a.availability_start).getTime() - new Date(b.availability_start).getTime()
@@ -15,6 +20,48 @@ export class AvailabilityGroupHelper {
         availabilityGroups.push(availabilityGroup)
       }
     }
+
+    availabilityGroups.sort((a, b) => {
+      return this.getArrivalDate(a).getTime() - this.getArrivalDate(b).getTime()
+    })
+
     return availabilityGroups
+  }
+
+  getArrivalDate(availabilityGroup: IProtelAvailabilityGroup) {
+    let arrivalDate = availabilityGroup.availabilities[0].availability_start
+    for (const availability of availabilityGroup.availabilities) {
+      if (availability.availability_start.getTime() < arrivalDate.getTime()) {
+        arrivalDate = availability.availability_start
+      }
+    }
+    return arrivalDate
+  }
+
+  getDepartureDate(availabilityGroup: IProtelAvailabilityGroup) {
+    let departureDate = availabilityGroup.availabilities[0].availability_start
+    for (const availability of availabilityGroup.availabilities) {
+      if (availability.availability_start.getTime() > departureDate.getTime()) {
+        departureDate = availability.availability_start
+        console.log(departureDate)
+      }
+    }
+    return this.dateHelper.addDays(departureDate, 1)
+  }
+
+  calculateTotalRate(
+    availabilityGroup: IProtelAvailabilityGroup,
+    guestsPerRoom: IGuestsPerRoom
+  ): number {
+    let roomRate = 0
+
+    for (const protelAvailability of availabilityGroup.availabilities) {
+      roomRate += this.ratesHelper.calculateActualRate(
+        protelAvailability.rates_data[0],
+        guestsPerRoom
+      )
+    }
+
+    return roomRate
   }
 }
