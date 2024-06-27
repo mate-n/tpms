7a39895eb613039ef3a7bcbe579f92e3ad5a50db
Sandro Räss <s.raess@me.com>
2024-06-27 23:39:45 +0200

    Add backend-middleware.

---
+++ backend-middleware/src/test/java/tpms/backend_middleware/BackendMiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package tpms.backend_middleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
