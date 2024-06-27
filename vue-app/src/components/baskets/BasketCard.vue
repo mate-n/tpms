7c9ad78fcceb9b4a87180e70fa4bcfcf9bd37861
Metaphysiker <s.raess@me.com>
2024-06-27 12:29:52 +0200

    Update BasketCard to show itineraryReservationConfirmedDialog when clicking book-button.

--- vue-app/src/components/baskets/BasketCard.vue
+++ vue-app/src/components/baskets/BasketCard.vue
@@ -38,13 +38,20 @@ const checkIfBookingIsPossible = () => {
     errorsDialog.value = true
   } else {
     errors.value = []
-    router.push('/itinerary-reservations/1')
-    emits('close')
+    itineraryConfirmedDialog.value = true
+    //router.push('/itinerary-reservations/1')
+    //emits('close')
   }
 }
 
 const errors = ref<string[]>([])
 const errorsDialog = ref(false)
+const itineraryConfirmedDialog = ref(false)
+
+const clickOnOkInConfirmedDialog = () => {
+  router.push('/itinerary-reservations')
+  emits('close')
+}
 </script>
 
 <template>
@@ -94,6 +101,30 @@ const errorsDialog = ref(false)
         <ul>
           <li v-for="error in errors" :key="error">{{ error }}</li>
         </ul>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+
+  <v-dialog v-model="itineraryConfirmedDialog" scrollable width="600">
+    <v-toolbar class="standard-dialog-card-toolbar fixed-toolbar">
+      <v-toolbar-title
+        ><span class="text-primary">Itinerary Reservation Confirmed</span></v-toolbar-title
+      >
+      <div
+        class="profiles-card-toolbar-button rounded-te"
+        @click="itineraryConfirmedDialog = false"
+      >
+        <v-icon size="large">mdi-close</v-icon>
+      </div>
+    </v-toolbar>
+    <v-divider class="profiles-card-divider"></v-divider>
+    <v-card class="rounded-t-0">
+      <v-card-text>
+        <div class="mb-5">Your itinerary reservation has been suceessfully booked.</div>
+        <div class="d-flex justify-end">
+          <v-btn class="me-2" @click="itineraryConfirmedDialog = false">Close</v-btn>
+          <v-btn class="primary-button" @click="clickOnOkInConfirmedDialog()">OK</v-btn>
+        </div>
       </v-card-text>
     </v-card>
   </v-dialog>
