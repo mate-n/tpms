ba16e8fc07f79c3ac9484d9ae4ab9a303a3ad199
Metaphysiker <s.raess@me.com>
2024-06-28 22:16:02 +0200

    Update build-and-run-locally.sh-script to build new south-africa-version.

--- infrastructure/demo/south-africa-server/build-and-run-locally.sh
+++ infrastructure/demo/south-africa-server/build-and-run-locally.sh
@@ -2,6 +2,8 @@
 
 docker build . -t realms --build-context root=../../../ \
     --build-arg VITE_API_BASE_URL=http://localhost:8001/ \
-    --build-arg BACKEND_PORT=8001
+    --build-arg VITE_API_BASE_URL2=http://localhost:8002/ \
+    --build-arg BACKEND_PORT=8001 \
+    --build-arg MIDDLEWARE_PORT=8002
 
-docker run -d -p 8000:8000 -p 8001:8001 --name realms --rm realms
+docker run -d -p 8000:8000 -p 8001:8001 -p 8002:8002 --name realms --rm realms
