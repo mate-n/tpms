c6f6a2ffdb0f09fa058f1d328de895036056a13d
Metaphysiker <s.raess@me.com>
2024-06-28 17:14:20 +0200

    Add ParkController.

---
+++ backend-middleware/src/main/java/tpms/backend_middleware/controllers/ParkController.java
@@ -0,0 +1,29 @@
+package tpms.backend_middleware.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tpms.backend_middleware.models.Park;
+import tpms.backend_middleware.repositories.ParkRepository;
+
+@RestController()
+@RequestMapping(path = "/api/parks", produces = "application/json")
+@CrossOrigin("*")
+public class ParkController {
+
+    private final ParkRepository repository;
+
+    @Autowired
+    public ParkController(ParkRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Iterable<Park> index() {
+        return repository.findAll();
+    }
+
+}
