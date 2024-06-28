5039257b5c0f8217d0855f725dc0772667fb2e1c
Metaphysiker <s.raess@me.com>
2024-06-28 18:08:41 +0200

    Add RegionService.

---
+++ vue-app/src/services/backend-middleware/RegionService.ts
@@ -0,0 +1,24 @@
+import { AxiosInstanceFactory2 } from '@/factories/AxiosInstanceFactory2'
+import type { IService } from '@/interfaces/IService'
+import type { IProtelRegion } from '@/shared/interfaces/protel/IProtelRegion'
+import type { AxiosStatic } from 'axios'
+
+export class RegionService implements IService {
+  axiosInstance: AxiosStatic
+  constructor(axios: AxiosStatic | undefined) {
+    this.axiosInstance = AxiosInstanceFactory2.createAxiosInstance(axios)
+  }
+
+  findAll() {
+    return new Promise<IProtelRegion[]>((resolve, reject) => {
+      this.axiosInstance
+        .get('v1/protel-regions')
+        .then((response: any) => {
+          resolve(response.data)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+}
