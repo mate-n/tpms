d88a776401f9b2ed3ac49252725db39ca1d964a8
Metaphysiker <s.raess@me.com>
2024-06-29 20:51:57 +0200

    Add FakeRegionController.

---
+++ backend-middleware/src/main/java/tpms/backend_middleware/controllers/FakeRegionController.java
@@ -0,0 +1,34 @@
+package tpms.backend_middleware.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tpms.backend_middleware.models.Region;
+import tpms.backend_middleware.repositories.RegionRepository;
+
+@RestController()
+@RequestMapping(path = "/fake/api/v1/regions", produces = "application/json")
+@CrossOrigin("*")
+public class FakeRegionController {
+    private final RegionRepository repository;
+
+    @Autowired
+    public FakeRegionController(RegionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Iterable<Region> index() {
+        return repository.findAll();
+    }
+
+    @GetMapping(path = "/createnew")
+    public Region save() {
+        Region region = new Region();
+        return repository.save(region);
+    }
+
+}
