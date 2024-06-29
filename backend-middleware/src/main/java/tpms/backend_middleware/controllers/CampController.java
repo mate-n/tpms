7a59dc6ffb70cde1a333ceb3071936fb29531e4e
Metaphysiker <s.raess@me.com>
2024-06-29 20:52:24 +0200

    Update CampController to use real api.

--- backend-middleware/src/main/java/tpms/backend_middleware/controllers/CampController.java
+++ backend-middleware/src/main/java/tpms/backend_middleware/controllers/CampController.java
@@ -8,20 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import tpms.backend_middleware.models.Camp;
 import tpms.backend_middleware.repositories.CampRepository;
+import tpms.backend_middleware.services.ParksAndCampsService;
 
 @RestController()
 @RequestMapping(path = "/api/v1/camps", produces = "application/json")
 @CrossOrigin("*")
 public class CampController {
-    private final CampRepository repository;
 
     @Autowired
     public CampController(CampRepository repository) {
-        this.repository = repository;
     }
 
     @GetMapping
     public Iterable<Camp> index() {
-        return repository.findAll();
+        ParksAndCampsService parksAndCampsService = new ParksAndCampsService();
+        try {
+            return parksAndCampsService.getCamps();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
