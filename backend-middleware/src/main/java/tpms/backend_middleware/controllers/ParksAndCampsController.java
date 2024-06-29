80d7ccb62af4f3bff2b10a78553daa0056ea2148
Sandro Räss <s.raess@me.com>
2024-06-29 13:14:02 +0200

    Update ParksAndCampsController to have getParks and getRegions.

--- backend-middleware/src/main/java/tpms/backend_middleware/controllers/ParksAndCampsController.java
+++ backend-middleware/src/main/java/tpms/backend_middleware/controllers/ParksAndCampsController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import tpms.backend_middleware.models.Park;
+import tpms.backend_middleware.models.Region;
 import tpms.backend_middleware.services.ParksAndCampsService;
 
 @RestController()
@@ -29,6 +31,26 @@ public class ParksAndCampsController {
             return parksAndCampsService.getParksAndCamps();
         } catch (Exception e) {
             return e.getMessage();
+        }
+    }
+
+    @GetMapping(path = "/getregions")
+    public Iterable<Region> getRegions() {
+        ParksAndCampsService parksAndCampsService = new ParksAndCampsService();
+        try {
+            return parksAndCampsService.getRegions();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @GetMapping(path = "/getparks")
+    public Iterable<Park> getParks() {
+        ParksAndCampsService parksAndCampsService = new ParksAndCampsService();
+        try {
+            return parksAndCampsService.getParks();
+        } catch (Exception e) {
+            return null;
         }
     }
 
