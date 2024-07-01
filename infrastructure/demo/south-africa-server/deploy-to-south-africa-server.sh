8a49a8c2c66dc751fa6a41bd19f67fe8c8a4cad4
Metaphysiker <s.raess@me.com>
2024-07-01 17:08:43 +0200

    Update deploy-to-south-africa-server script.

--- infrastructure/demo/south-africa-server/deploy-to-south-africa-server.sh
+++ infrastructure/demo/south-africa-server/deploy-to-south-africa-server.sh
@@ -6,16 +6,18 @@ docker build . -t realms --build-context root=../../../ \
     --build-arg BACKEND_PORT=8001 \
     --build-arg MIDDLEWARE_PORT=8002
 
-#docker build . -t realms --build-context root=../../../ \
-#    --build-arg VITE_API_BASE_URL=https://tpms-api.realms.ch/ \
-#    --build-arg BACKEND_PORT=8001
-
 ssh tpms-user@41.76.108.121 << EOF
-    docker stop realms
+    cd tpms-frontend
+    git stash
+    git pull origin demo
+    cd infrastructure/demo/south-africa-server/
+    docker compose down
 EOF
 
 docker save realms | bzip2 | ssh tpms-user@41.76.108.121 docker load
 
+
 ssh tpms-user@41.76.108.121 << EOF
-    docker run -d -p 8000:8000 -p 8001:8001 --name realms --rm realms
+    cd infrastructure/demo/south-africa-server/
+    docker compose up -d
 EOF
