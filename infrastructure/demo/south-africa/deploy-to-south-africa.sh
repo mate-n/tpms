1c100612ae0569b2f9d64a6cf614772f4ac8cd51
Sandro Räss <s.raess@me.com>
2024-07-01 23:54:53 +0200

    Add deploy-to-south-africa.sh.

---
+++ infrastructure/demo/south-africa/deploy-to-south-africa.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker compose --file docker-compose.build.yml build
+
+docker save tpms-frontend-south-africa-vue | bzip2 | pv | ssh tpms-user@41.76.108.121 docker load
+
+docker save tpms-frontend-south-africa-backend-middleware | bzip2 | pv | ssh tpms-user@41.76.108.121 docker load
+
+docker save tpms-frontend-south-africa-nest | bzip2 | pv | ssh tpms-user@41.76.108.121 docker load
+
+scp docker-compose.remote.yml tpms-user@41.76.108.121:/home/tpms-user/tpms-frontend-south-africa
+
+scp .env tpms-user@41.76.108.121:/home/tpms-user/tpms-frontend-south-africa
+
+ssh tpms-user@41.76.108.121 << EOF
+    cd /home/tpms-user/tpms-frontend-south-africa
+    docker compose --file docker-compose.remote.yml down
+    docker compose --file docker-compose.remote.yml up -d
+    docker system prune -f
+EOF
