b01cf6a89121aa50d285b30ad67e539fdabea947
Sandro Räss <s.raess@me.com>
2024-07-02 21:47:08 +0200

    Add ProfileCreatePostBody.

---
+++ backend-middleware/src/main/java/tpms/backend_middleware/classes/ProfileCreatePostBody.java
@@ -0,0 +1,26 @@
+package tpms.backend_middleware.classes;
+
+public class ProfileCreatePostBody {
+    public String NamePrefix;
+    public String GivenName;
+    public String MiddleName;
+    public String Surname;
+    public String BirthDate;
+    public String Nationality;
+    public String Language;
+    public String Mobile;
+    public String Telephone;
+    public String Email;
+    public String AddressLine1;
+    public String AddressLine2;
+    public String AddressLine3;
+    public String CityName;
+    public String PostalCode;
+    public String CountryName;
+    public String CitizenCountryName;
+    public String SAIdNumber;
+    public String PassportNumber;
+    public String PassportDateOfIssue;
+    public String PassportExpiryDate;
+    public String PassportCountry;
+}
