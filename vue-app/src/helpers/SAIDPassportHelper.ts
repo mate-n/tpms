1ad5f90fd6381ec83523f3778c9f928156b526ab
Metaphysiker <s.raess@me.com>
2024-07-02 11:22:05 +0200

    Add SAIDPassportHelper.

---
+++ vue-app/src/helpers/SAIDPassportHelper.ts
@@ -0,0 +1,9 @@
+export class SAIDPassportHelper {
+  generateStartOfSAIDNumber(dateOfBirth: Date) {
+    const yy = dateOfBirth.getFullYear().toString().slice(-2)
+    const mm = (dateOfBirth.getMonth() + 1).toString().padStart(2, '0')
+    const dd = dateOfBirth.getDate().toString().padStart(2, '0')
+    const digitafterFirst6Digits = 5
+    return parseInt(yy + mm + dd + digitafterFirst6Digits)
+  }
+}
