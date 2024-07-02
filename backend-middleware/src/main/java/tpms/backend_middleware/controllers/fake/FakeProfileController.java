832b64f992a6028ff03a38727c74123e0008adc1
Metaphysiker <s.raess@me.com>
2024-07-02 19:41:00 +0200

    Add FakeProfileController.

---
+++ backend-middleware/src/main/java/tpms/backend_middleware/controllers/fake/FakeProfileController.java
@@ -0,0 +1,32 @@
+package tpms.backend_middleware.controllers.fake;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tpms.backend_middleware.classes.ProfileLookUpPostBody;
+import tpms.backend_middleware.models.Profile;
+import tpms.backend_middleware.repositories.ProfileRepository;
+
+@RestController()
+@RequestMapping(path = "/fake/api/v1/profiles", produces = "application/json")
+@CrossOrigin("*")
+public class FakeProfileController {
+
+    private final ProfileRepository repository;
+
+    @Autowired
+    public FakeProfileController(ProfileRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping(path = "/lookup")
+    public Iterable<Profile> lookup(@RequestBody ProfileLookUpPostBody profileLookUpPostBody) throws IOException {
+        return repository.findAll();
+    }
+}
