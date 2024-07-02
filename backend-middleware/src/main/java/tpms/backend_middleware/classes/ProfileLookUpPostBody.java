0dd7f200c3dfc4fdac66b612bbd077d32551c7ae
Metaphysiker <s.raess@me.com>
2024-07-02 19:25:34 +0200

    Add ProfileLookupPostBody.

---
+++ backend-middleware/src/main/java/tpms/backend_middleware/classes/ProfileLookUpPostBody.java
@@ -0,0 +1,14 @@
+package tpms.backend_middleware.classes;
+
+public class ProfileLookUpPostBody {
+    public String surname;
+    public String name;
+    public String email;
+    public String mobile;
+    public String SAId;
+    public String passportno;
+    public String roomseekerclientcode;
+    public int tpmsProfileID;
+    public String wildcardnumber;
+    public String loyaltynumber;
+}
