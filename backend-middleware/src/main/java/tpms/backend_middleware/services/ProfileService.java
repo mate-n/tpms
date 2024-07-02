b6cea9235f75b20a42829915b06f73f7e7ca2bf9
Sandro Räss <s.raess@me.com>
2024-07-02 21:46:19 +0200

    Add create to ProfileService.

--- backend-middleware/src/main/java/tpms/backend_middleware/services/ProfileService.java
+++ backend-middleware/src/main/java/tpms/backend_middleware/services/ProfileService.java
@@ -2,7 +2,7 @@ package tpms.backend_middleware.services;
 
 import java.io.IOException;
 import java.util.ArrayList;
-
+import java.util.concurrent.TimeUnit;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,6 +12,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import tpms.backend_middleware.classes.ProfileCreatePostBody;
 import tpms.backend_middleware.classes.ProfileLookUpPostBody;
 import tpms.backend_middleware.models.Profile;
 
@@ -26,6 +27,7 @@ public class ProfileService {
                                 MediaType.parse("application/json"));
 
                 OkHttpClient client = new OkHttpClient().newBuilder()
+                                .pingInterval(3, TimeUnit.SECONDS)
                                 .build();
                 Request request = new Request.Builder()
                                 .url("https://ankerws.ankerdata.co.za/tpmsprofileservice/v1/lookup.php")
@@ -40,5 +42,27 @@ public class ProfileService {
                                 new TypeReference<ArrayList<Profile>>() {
                                 });
                 return profiles;
+        }
+
+        public String create(ProfileCreatePostBody profileCreatePostBody) throws IOException {
+                ObjectMapper objectMapper = new ObjectMapper().configure(
+                                DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+                                false);
+                String profileCreatePostBodyAsString = objectMapper.writeValueAsString(profileCreatePostBody);
+                System.out.println(profileCreatePostBodyAsString);
+                RequestBody body = RequestBody.create(profileCreatePostBodyAsString,
+                                MediaType.parse("application/json"));
+
+                OkHttpClient client = new OkHttpClient().newBuilder()
+                                .build();
+                Request request = new Request.Builder()
+                                .url("https://ankerws.ankerdata.co.za/IProfileService/create/v1/index.php")
+                                .addHeader("Authorization", "Bearer 7cca0f181183802bec0e3434c00041afe870d516")
+                                .post(body)
+                                .build();
+
+                Response response = client.newCall(request).execute();
+                var reponseString = response.body().string();
+                return reponseString;
         }
 }
