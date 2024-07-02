de5467730b63e845602edcca7581b99beaddb3c6
Metaphysiker <s.raess@me.com>
2024-07-02 20:17:17 +0200

    Add ProfileLookUpPostBodyHelper.

--- vue-app/src/helpers/ProfileLookUpPostBodyHelper.ts
+++ vue-app/src/helpers/ProfileLookUpPostBodyHelper.ts
@@ -1,31 +1,19 @@
 import { Profile } from '@/shared/classes/Profile'
-import { ProfileCommunication } from '@/shared/classes/ProfileCommunication'
 import type { IProfileLookUpPostBody } from '@/shared/interfaces/profiles/IProfileLookUpPostBody'
 
-export class ProfileSearchHelper {
+export class ProfileLookUpPostBodyHelper {
   convertToProfile(profileLookUpPostBody: IProfileLookUpPostBody): Profile {
     const profile = new Profile()
     if (profileLookUpPostBody.surname) profile.surname = profileLookUpPostBody.surname
     if (profileLookUpPostBody.name) profile.name = profileLookUpPostBody.name
-    if (profileLookUpPostBody.email) {
-      const communication = new ProfileCommunication()
-      communication.communicationTypeID = 147
-      communication.communicationTypeName = 'E-Mail'
-      communication.value = profileLookUpPostBody.email
-      communication.primary = true
-      profile.communications.push(communication)
-    }
-    if (profileLookUpPostBody.mobile) {
-      profile.phone = profileLookUpPostBody.mobile
-      const communication = new ProfileCommunication()
-      communication.communicationTypeID = 160
-      communication.communicationTypeName = 'Mobile'
-      communication.value = profileLookUpPostBody.mobile
-      communication.primary = true
-      profile.communications.push(communication)
-    }
+    if (profileLookUpPostBody.email) profile.email = profileLookUpPostBody.email
+    if (profileLookUpPostBody.mobile) profile.mobile = profileLookUpPostBody.mobile
     if (profileLookUpPostBody.sAID) profile.sAId = profileLookUpPostBody.sAID
     if (profileLookUpPostBody.passportno) profile.passportno = profileLookUpPostBody.passportno
+    if (profileLookUpPostBody.wildcardnumber)
+      profile.wildcardnumber = profileLookUpPostBody.wildcardnumber
+    if (profileLookUpPostBody.loyaltynumber)
+      profile.loyaltynumber = profileLookUpPostBody.loyaltynumber
     if (profileLookUpPostBody.roomseekerclientcode)
       profile.roomSeekerClientCode = profileLookUpPostBody.roomseekerclientcode
     if (profileLookUpPostBody.tpmsProfileID)
