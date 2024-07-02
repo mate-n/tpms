175d06cfae41a07ae92b2d6ea26983f2438bcac8
Metaphysiker <s.raess@me.com>
2024-07-02 19:26:16 +0200

    Add Profile.

--- backend-middleware/src/main/java/tpms/backend_middleware/models/Profile.java
+++ backend-middleware/src/main/java/tpms/backend_middleware/models/Profile.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 import tpms.backend_middleware.interfaces.IModel;
 
@@ -12,7 +14,33 @@ import tpms.backend_middleware.interfaces.IModel;
 @Document
 public class Profile implements IModel {
     @Id
+    @JsonIgnoreProperties(ignoreUnknown = true)
     private String id;
+    private int profileID;
+    private String title;
+    private String initial;
+    private String surname;
+    private String name;
+    private String email;
+    private String telephone;
+    private String mobile;
+    private String address;
+    private String postalCode;
+    private String city;
+    private String country;
+    private String dateofbirth;
+    private String gender;
+    private String roomseekerclientcode;
+    private String SAId;
+    private String passportno;
+    private String countryofbirth;
+    private String wildcardnumber;
+    private String loyaltynumber;
+    private String marketing;
+    private String reservationsms;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
     private Date createdAt = new Date();
+    @JsonIgnoreProperties(ignoreUnknown = true)
     private Date updatedAt = new Date();
 }
