08fb236ed73f3866b1d34401f7cfd763c851802b
Metaphysiker <s.raess@me.com>
2024-07-02 19:29:27 +0200

    Update ProfileSearchHelper to assign values to profileLookUpPostBody.

--- vue-app/src/helpers/ProfileSearchHelper.ts
+++ vue-app/src/helpers/ProfileSearchHelper.ts
@@ -1,34 +1,35 @@
 import { Profile } from '@/shared/classes/Profile'
 import { ProfileCommunication } from '@/shared/classes/ProfileCommunication'
-import type { IProfileSearch } from '@/shared/interfaces/profiles/IProfileSearch'
+import type { IProfileLookUpPostBody } from '@/shared/interfaces/profiles/IProfileLookUpPostBody'
 
 export class ProfileSearchHelper {
-  convertToProfile(profileSearch: IProfileSearch): Profile {
+  convertToProfile(profileLookUpPostBody: IProfileLookUpPostBody): Profile {
     const profile = new Profile()
-    if (profileSearch.surname) profile.surname = profileSearch.surname
-    if (profileSearch.name) profile.name = profileSearch.name
-    if (profileSearch.email) {
+    if (profileLookUpPostBody.surname) profile.surname = profileLookUpPostBody.surname
+    if (profileLookUpPostBody.name) profile.name = profileLookUpPostBody.name
+    if (profileLookUpPostBody.email) {
       const communication = new ProfileCommunication()
       communication.communicationTypeID = 147
       communication.communicationTypeName = 'E-Mail'
-      communication.value = profileSearch.email
+      communication.value = profileLookUpPostBody.email
       communication.primary = true
       profile.communications.push(communication)
     }
-    if (profileSearch.mobile) {
-      profile.phone = profileSearch.mobile
+    if (profileLookUpPostBody.mobile) {
+      profile.phone = profileLookUpPostBody.mobile
       const communication = new ProfileCommunication()
       communication.communicationTypeID = 160
       communication.communicationTypeName = 'Mobile'
-      communication.value = profileSearch.mobile
+      communication.value = profileLookUpPostBody.mobile
       communication.primary = true
       profile.communications.push(communication)
     }
-    if (profileSearch.saIDNumber) profile.saIDNumber = profileSearch.saIDNumber
-    if (profileSearch.passportNumber) profile.passportNumber = profileSearch.passportNumber
-    if (profileSearch.roomSeekerClientCode)
-      profile.roomSeekerClientCode = profileSearch.roomSeekerClientCode
-    if (profileSearch.tpmsProfileID) profile.tpmsProfileID = profileSearch.tpmsProfileID
+    if (profileLookUpPostBody.sAID) profile.sAId = profileLookUpPostBody.sAID
+    if (profileLookUpPostBody.passportno) profile.passportno = profileLookUpPostBody.passportno
+    if (profileLookUpPostBody.roomseekerclientcode)
+      profile.roomSeekerClientCode = profileLookUpPostBody.roomseekerclientcode
+    if (profileLookUpPostBody.tpmsProfileID)
+      profile.tpmsProfileID = profileLookUpPostBody.tpmsProfileID
     return profile
   }
 }
