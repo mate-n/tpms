7483c8421a151048328669ed56f64d0d04c274d4
Metaphysiker <s.raess@me.com>
2024-07-02 19:25:04 +0200

    Add ProfileService.

---
+++ vue-app/src/services/backend-middleware/ProfileService.ts
@@ -0,0 +1,41 @@
+import { AxiosInstanceFactory2 } from '@/factories/AxiosInstanceFactory2'
+import type { IService } from '@/interfaces/IService'
+import type { IProfile } from '@/shared/interfaces/profiles/IProfile'
+import type { IProfileLookUpPostBody } from '@/shared/interfaces/profiles/IProfileLookUpPostBody'
+import type { AxiosStatic } from 'axios'
+
+export class ProfileService implements IService {
+  axiosInstance: any
+  constructor(axios: AxiosStatic | undefined) {
+    this.axiosInstance = AxiosInstanceFactory2.createAxiosInstance(axios)
+  }
+
+  getAll() {
+    return new Promise<IProfile[]>((resolve, reject) => {
+      this.axiosInstance
+        .get('api/v1/profiles/lookup')
+        .then((response: any) => {
+          resolve(response.data)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+
+  lookup(profileLookUpPostBody: IProfileLookUpPostBody) {
+    return new Promise<IProfile[]>((resolve, reject) => {
+      this.axiosInstance
+        .post('api/v1/profiles/lookup', profileLookUpPostBody)
+        .then((response: any) => {
+          if (response.data[0].profileID === -1) {
+            resolve([])
+          }
+          resolve(response.data)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+}
