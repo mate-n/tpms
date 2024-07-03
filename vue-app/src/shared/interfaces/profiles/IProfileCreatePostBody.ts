ad0c5e6ef233ba7a0566c20e8af5b5218a51b91d
Metaphysiker <s.raess@me.com>
2024-07-03 09:12:39 +0200

    Add IProfileCreatePostBody.

---
+++ vue-app/src/shared/interfaces/profiles/IProfileCreatePostBody.ts
@@ -0,0 +1,24 @@
+export interface IProfileCreatePostBody {
+  NamePrefix: string
+  GivenName: string
+  MiddleName: string
+  Surname: string
+  BirthDate: string
+  Nationality: string
+  Language: string
+  Mobile: string
+  Telephone: string
+  Email: string
+  AddressLine1: string
+  AddressLine2: string
+  AddressLine3: string
+  CityName: string
+  PostalCode: string
+  CountryName: string
+  CitizenCountryName: string
+  SAIdNumber: string
+  PassportNumber: string
+  PassportDateOfIssue: string
+  PassportExpiryDate: string
+  PassportCountry: string
+}
