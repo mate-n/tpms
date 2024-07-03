ee4ea095f884d50f052a3ae3dc59845c1683026e
Sandro Räss <s.raess@me.com>
2024-07-03 10:20:54 +0200

    Update ProfileController to remove repository.

--- backend-middleware/src/main/java/tpms/backend_middleware/controllers/ProfileController.java
+++ backend-middleware/src/main/java/tpms/backend_middleware/controllers/ProfileController.java
@@ -1,11 +1,9 @@
 package tpms.backend_middleware.controllers;
 
 import java.io.IOException;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,18 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 import tpms.backend_middleware.classes.ProfileCreatePostBody;
 import tpms.backend_middleware.classes.ProfileLookUpPostBody;
 import tpms.backend_middleware.models.Profile;
-import tpms.backend_middleware.repositories.ProfileRepository;
 import tpms.backend_middleware.services.ProfileService;
 
 @RestController()
 @RequestMapping(path = "api/v1/profiles", produces = "application/json", consumes = "application/json")
 @CrossOrigin("*")
 public class ProfileController {
-    private final ProfileRepository repository;
 
     @Autowired
-    public ProfileController(ProfileRepository repository) {
-        this.repository = repository;
+    public ProfileController() {
     }
 
     @PostMapping(path = "/lookup")
@@ -39,12 +34,4 @@ public class ProfileController {
         ProfileService profileService = new ProfileService();
         return profileService.create(profileCreatePostBody);
     }
-
-    @GetMapping(path = "/createnew")
-    public Profile save() {
-        Profile profile = new Profile();
-        profile.setUpdatedAt(new Date());
-        return repository.save(profile);
-    }
-
 }
