6de092eaa4a3c1df380dec0a6d00923f72da3fdc
Metaphysiker <s.raess@me.com>
2024-07-03 17:57:02 +0200

    Add get to ProfileService.

--- vue-app/src/services/backend-middleware/ProfileService.ts
+++ vue-app/src/services/backend-middleware/ProfileService.ts
@@ -1,22 +1,47 @@
 import { AxiosInstanceFactory2 } from '@/factories/AxiosInstanceFactory2'
 import type { IService } from '@/interfaces/IService'
+import { ProfileLookUpPostBody } from '@/shared/classes/ProfileLookUpPostBody'
+import { ProfileConverter } from '@/shared/converters/ProfileConverter'
 import type { IProfile } from '@/shared/interfaces/profiles/IProfile'
 import type { IProfileCreatePostBody } from '@/shared/interfaces/profiles/IProfileCreatePostBody'
 import type { IProfileLookUpPostBody } from '@/shared/interfaces/profiles/IProfileLookUpPostBody'
 import type { AxiosStatic } from 'axios'
 
 export class ProfileService implements IService {
+  profileConverter = new ProfileConverter()
   axiosInstance: any
   constructor(axios: AxiosStatic | undefined) {
     this.axiosInstance = AxiosInstanceFactory2.createAxiosInstance(axios)
   }
 
+  get(profileId: number) {
+    return new Promise<IProfile | undefined>((resolve, reject) => {
+      const profileLookUpPostBody = new ProfileLookUpPostBody()
+      profileLookUpPostBody.tpmsProfileID = profileId
+      this.axiosInstance
+        .post('api/v1/profiles/lookup', profileLookUpPostBody)
+        .then((response: any) => {
+          if (response.data[0] && response.data[0].profileID === -1) {
+            resolve(undefined)
+          }
+          const profile = this.profileConverter.convertProfileFromAPIToProfile(response.data[0])
+          resolve(profile)
+        })
+        .catch((e: any) => {
+          reject(e)
+        })
+    })
+  }
+
   getAll() {
     return new Promise<IProfile[]>((resolve, reject) => {
       this.axiosInstance
-        .get('api/v1/profiles/lookup')
+        .post('api/v1/profiles/lookup', {})
         .then((response: any) => {
-          resolve(response.data)
+          const profiles = response.data.map((profile: any) => {
+            return this.profileConverter.convertProfileFromAPIToProfile(profile)
+          })
+          resolve(profiles)
         })
         .catch((e: any) => {
           reject(e)
@@ -32,7 +57,10 @@ export class ProfileService implements IService {
           if (response.data[0] && response.data[0].profileID === -1) {
             resolve([])
           }
-          resolve(response.data)
+          const profiles = response.data.map((profile: any) => {
+            return this.profileConverter.convertProfileFromAPIToProfile(profile)
+          })
+          resolve(profiles)
         })
         .catch((e: any) => {
           reject(e)
@@ -45,20 +73,8 @@ export class ProfileService implements IService {
       this.axiosInstance
         .post('api/v1/profiles', profileCreatePostBody)
         .then((response: any) => {
-          resolve(response.data)
-        })
-        .catch((e: any) => {
-          reject(e)
-        })
-    })
-  }
-
-  update(profileCreatePostBody: IProfileCreatePostBody) {
-    return new Promise<IProfile>((resolve, reject) => {
-      this.axiosInstance
-        .put('api/v1/profiles', profileCreatePostBody)
-        .then((response: any) => {
-          resolve(response.data)
+          const profile = this.profileConverter.convertProfileFromAPIToProfile(response)
+          resolve(profile)
         })
         .catch((e: any) => {
           reject(e)
