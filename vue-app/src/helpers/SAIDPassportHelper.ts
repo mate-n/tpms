cc8d4181d0159740c2a967f46faed36faaf20ace
Metaphysiker <s.raess@me.com>
2024-07-04 12:48:05 +0200

    Add extractBirthDateFromSAIDNumber and extractGenderFromSAIDNumber to SAIDPAssportHelper.

--- vue-app/src/helpers/SAIDPassportHelper.ts
+++ vue-app/src/helpers/SAIDPassportHelper.ts
@@ -6,4 +6,27 @@ export class SAIDPassportHelper {
     const digitafterFirst6Digits = 5
     return yy + mm + dd + digitafterFirst6Digits
   }
+
+  extractBirthdateFromSAIDNumber(said: string): Date {
+    const dateOfBirth = new Date()
+    const yy = parseInt(said.slice(0, 2))
+    const mm = said.slice(2, 4)
+    const dd = said.slice(4, 6)
+    if (yy > 13) {
+      dateOfBirth.setFullYear(1900 + yy)
+    } else {
+      dateOfBirth.setFullYear(2000 + yy)
+    }
+    dateOfBirth.setMonth(parseInt(mm) - 1)
+    dateOfBirth.setDate(parseInt(dd))
+    return dateOfBirth
+  }
+
+  extractGenderFromSAIDNumber(said: string): string {
+    const genderNumber = said.slice(6, 10)
+    if (parseInt(genderNumber) < 5000) {
+      return 'FEMALE'
+    }
+    return 'MALE'
+  }
 }
