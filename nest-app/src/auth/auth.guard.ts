0d4bb9d2c5277d464da404ecbb172b5374c111bd
Metaphysiker <s.raess@me.com>
2024-07-04 12:49:11 +0200

    Update auth.guard and auth.module to use constants.example.

--- nest-app/src/auth/auth.guard.ts
+++ nest-app/src/auth/auth.guard.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Request } from 'express'
-import { jwtConstants } from './constants'
+import { jwtConstants } from './constants.example'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
