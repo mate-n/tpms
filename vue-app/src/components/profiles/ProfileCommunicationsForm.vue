16ad9e132aa47d81d0120d3e2b7d54f452e446b7
quanghuyhh <k44a5asi@gmail.com>
2024-07-05 21:59:32 +0700

    Add initial empty row when open modal profile contact details

--- vue-app/src/components/profiles/ProfileCommunicationsForm.vue
+++ vue-app/src/components/profiles/ProfileCommunicationsForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onBeforeMount, ref, watch } from 'vue'
+import {nextTick, onBeforeMount, onMounted, ref, watch} from 'vue'
 import { ProfileCommunicationService } from '@/services/profiles/ProfileCommunicationService'
 import ProfileCommunicationForm from './ProfileCommunicationForm.vue'
 import { IdentityHelper } from '@/helpers/IdentityHelper'
@@ -35,6 +35,11 @@ const profileCommunicationService = new ProfileCommunicationService(axios)
 
 onBeforeMount(() => {
   reloadProfileCommunications()
+})
+
+onMounted(async () => {
+  await nextTick()
+  addProfileCommunication()
 })
 
 const saveProfileCommunications = () => {
