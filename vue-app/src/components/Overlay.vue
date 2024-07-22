4435e83bda485bdca4f571299a7e84192f60bf78
Phong Ho <phonghophamminh@gmail.com>
2024-07-22 15:59:16 +0200

    286 Overlay for waiting

---
+++ vue-app/src/components/Overlay.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+  zIndex: { type: Number, default: 1100 },
+  fixed: { type: Boolean, default: true },
+  title: { type: String, default: 'Please wait…' }
+})
+</script>
+
+<template>
+  <div
+    class="top-0 left-0 d-flex justify-center align-center w-100 h-screen text-white"
+    :class="[fixed ? 'position-fixed' : 'position-absolute']"
+    :style="`z-index: ${zIndex}; background: rgba(0, 0, 0, 0.3);backdrop-filter: blur(2px);`"
+  >
+    <p class="text-h5 font-weight-bold">{{ title }}</p>
+  </div>
+</template>
