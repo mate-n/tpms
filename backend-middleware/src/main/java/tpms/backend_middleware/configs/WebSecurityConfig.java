6a9ec9cf3d35c8e4dd937b22cb4fa9024333aec9
Nguyen Duc Vuong <kojca.vsso@gmail.com>
2024-08-06 00:23:58 +0700

    Update remove authentication api create user

--- backend-middleware/src/main/java/tpms/backend_middleware/configs/WebSecurityConfig.java
+++ backend-middleware/src/main/java/tpms/backend_middleware/configs/WebSecurityConfig.java
@@ -34,6 +34,7 @@ public class WebSecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/authenticate").permitAll()
+                .requestMatchers("/api/setup/create-users").permitAll()
                 .requestMatchers("/api/v1/admin/**").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
             )
